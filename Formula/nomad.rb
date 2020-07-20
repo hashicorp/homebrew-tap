661e6e8d537e4d93d4293a5444823265681c0212
Omar Khawaja <Omar-Khawaja@users.noreply.github.com>
2020-07-20 15:52:45 -0400

    Update nomad.rb
    fix typos

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -1,8 +1,8 @@
 class Nomad < Formula
-    desc: "Nomad"
-    homepage: "https://www.nomadproject.io/"
-    version: "0.11.3"
-    bottle: unneeded
+    desc "Nomad"
+    homepage "https://www.nomadproject.io/"
+    version "0.11.3"
+    bottle :unneeded
     if OS.mac?
       url "https://releases.hashicorp.com/nomad/0.11.3/nomad_0.11.3_darwin_amd64.zip"
       sha256 "5c4087d6e79e1465bd9464d17d196b489abfb0c3c1290fc56ab88843d92a08da"
