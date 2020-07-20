fefa0ab8295e21fc3b1871e3d0b3a43936f9ffbd
Chloe Cota <cotarg@users.noreply.github.com>
2020-07-20 12:52:52 -0700

    Update consul.rb

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -1,8 +1,8 @@
 class Consul < Formula
-  desc: "Consul"
-  homepage: "https://www.consul.io"
-  version: "1.7.4"
-  bottle: unneeded
+  desc "Consul"
+  homepage "https://www.consul.io"
+  version "1.7.4"
+  bottle :unneeded
   if OS.mac?
     url "https://releases.hashicorp.com/consul/1.7.4/consul_1.7.4_darwin_amd64.zip"
     sha256 "ec089afe66c7c49237d6ec1beca02257f0fe861d0615b2f06c3243cc9fd4e142"
