dffee55ef4a7143439c8fc3eb5688b287961e9e9
Omar Khawaja <Omar-Khawaja@users.noreply.github.com>
2020-07-21 18:17:49 +0000

    vault 1.4.3

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -1,16 +1,16 @@
 class Vault < Formula
   desc "Vault"
   homepage "https://www.vaultproject.io"
-  version "1.4.2"
+  version "1.4.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://releases.hashicorp.com/vault/1.4.2/vault_1.4.2_darwin_amd64.zip"
+    url "https://releases.hashicorp.com/vault/1.4.3/vault_1.4.3_darwin_amd64.zip"
     sha256 "30d08188fd251a4e214c3a320472ee0b27b0499522860b7061bf0e49bc5fc466"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://releases.hashicorp.com/vault/1.4.2/vault_1.4.2_linux_amd64.zip"
-      sha256 "f2bca89cbffb8710265eb03bc9452cc316b03338c411ba8453ffe7419390b8f1"
+      url "https://releases.hashicorp.com/vault/1.4.3/vault_1.4.3_linux_amd64.zip"
+      sha256 "f486da4f6d08e42eb2c17e95cf36cc7f9d9c0e7cc8ced06ce3fca7c3abd7db3d"
     end
   end
 
