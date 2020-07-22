4a794a6a95a3b51b371e53800485d5a71ca4ec67
hashibot <hashibot-feedback@hashicorp.com>
2020-07-22 21:06:48 +0100

    Brew formula update for terraform-ls version v0.5.4

--- Formula/terraform-ls.rb
+++ Formula/terraform-ls.rb
@@ -2,16 +2,16 @@
 class TerraformLs < Formula
   desc "Terraform Language Server"
   homepage "https://github.com/hashicorp/terraform-ls"
-  version "0.5.3"
+  version "0.5.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://releases.hashicorp.com/terraform-ls/0.5.3/terraform-ls_0.5.3_darwin_amd64.zip"
-    sha256 "f1f2b2470c288fa6dd90a23b0a4c95de39b23272df3a48bd997a5f50b938a069"
+    url "https://releases.hashicorp.com/terraform-ls/0.5.4/terraform-ls_0.5.4_darwin_amd64.zip"
+    sha256 "208dee52dc19b5d9263d3714538e8173ad77b0951dc23defb1c0bca2cba738f8"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://releases.hashicorp.com/terraform-ls/0.5.3/terraform-ls_0.5.3_linux_amd64.zip"
-      sha256 "0af28e88d0a7fd70cd5591e8c6ee69fd541a8b91d2b644ebb422e44c28509634"
+      url "https://releases.hashicorp.com/terraform-ls/0.5.4/terraform-ls_0.5.4_linux_amd64.zip"
+      sha256 "bbcbf52deb872a2c0ee1073f80c1aa8858f7ad6dccd6331181ca40c57ecdb8ba"
     end
   end
   
