3224faa24a87a1905f85ff1ca120e7f6d034a8ad
chloe (@cotarg) <chloe@hashicorp.com>
2020-07-22 11:24:17 -0700

    fixing formulae

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -3,15 +3,18 @@ class Consul < Formula
   homepage "https://www.consul.io"
   version "1.7.4"
   bottle :unneeded
-  if OS.mac?
-    url "https://releases.hashicorp.com/consul/1.7.4/consul_1.7.4_darwin_amd64.zip"
-    sha256 "ec089afe66c7c49237d6ec1beca02257f0fe861d0615b2f06c3243cc9fd4e142"
-  end
+
+  url "https://releases.hashicorp.com/consul/1.7.4/consul_1.7.4_darwin_amd64.zip"
+  sha256 "ec089afe66c7c49237d6ec1beca02257f0fe861d0615b2f06c3243cc9fd4e142"
+  
   conflicts_with "consul"
+  
   def install
     bin.install "consul"
   end
+  
   test do
     system "#{bin}/consul --version"
   end
+
 end
--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -3,14 +3,17 @@ class Nomad < Formula
     homepage "https://www.nomadproject.io/"
     version "0.11.3"
     bottle :unneeded
-    if OS.mac?
-      url "https://releases.hashicorp.com/nomad/0.11.3/nomad_0.11.3_darwin_amd64.zip"
-      sha256 "5c4087d6e79e1465bd9464d17d196b489abfb0c3c1290fc56ab88843d92a08da"
-    end
+
+
+    url "https://releases.hashicorp.com/nomad/0.11.3/nomad_0.11.3_darwin_amd64.zip"
+    sha256 "5c4087d6e79e1465bd9464d17d196b489abfb0c3c1290fc56ab88843d92a08da"
+
     conflicts_with "nomad"
+
     def install
       bin.install "nomad"
     end
+
     test do
       system "#{bin}/nomad --version"
     end
