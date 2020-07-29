6903754b40682dff058ca27c1cf00950d4e90b8f
Kyle Penfound <kpenfound11@gmail.com>
2020-07-29 14:34:55 -0400

    rename hr to hb

--- util/lambda_trigger/releases.go
+++ util/lambda_trigger/releases.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	hr "github.com/gulducat/hashi-bin/types"
+	hb "github.com/gulducat/hashi-bin/types"
 )
 
 const ReleasesURL = "https://releases.hashicorp.com/index.json"
 
-func getLatestVersion(productName string) (*hr.Version, error) {
-	index, err := hr.NewIndex(ReleasesURL)
+func getLatestVersion(productName string) (*hb.Version, error) {
+	index, err := hb.NewIndex(ReleasesURL)
 	if err != nil {
-		return &hr.Version{}, err
+		return &hb.Version{}, err
 	}
 	product, err := index.GetProduct(productName)
 	if err != nil {
-		return &hr.Version{}, err
+		return &hb.Version{}, err
 	}
 	return product.LatestVersion(), nil
 }
