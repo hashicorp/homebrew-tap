5f0b3db6e71db91908b93ed15a794ef5fc108b2b
hc-espd-packager <68665936+hc-espd-packager@users.noreply.github.com>
2020-08-28 18:11:32 +0000

    packer 1.6.2

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -2,9 +2,9 @@ class Packer < Formula
   desc "Packer"
   homepage "https://www.packer.io/"
 
-  url "https://releases.hashicorp.com/packer/1.6.0/packer_1.6.0_darwin_amd64.zip"
-  version "1.6.0"
-  sha256 "037572085722adbeac22b69940330f86b1e82547056740cf1cc42636b057d2a2"
+  url "https://releases.hashicorp.com/packer/1.6.2/packer_1.6.2_darwin_amd64.zip"
+  version "1.6.2"
+  sha256 "2b5e8c76ae280c72fe33b2ac6511c24d2e0c02df9d437d2e4a5efbe988f71e2b"
   bottle :unneeded
 
   conflicts_with "packer"
