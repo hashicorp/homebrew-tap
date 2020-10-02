e0d286ff604a26feb24bc12265a438ab2d126e45
Sergey Petrov <git@pr0ger.org>
2020-10-03 01:18:10 +0300

    Add support for brew services

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -13,6 +13,40 @@ class Nomad < Formula
     bin.install "nomad"
   end
 
+  plist_options manual: "nomad agent -dev"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/nomad</string>
+            <string>agent</string>
+            <string>-dev</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/nomad.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/nomad.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
+
   test do
     system "#{bin}/nomad --version"
   end
--- Formula/vault.rb
+++ Formula/vault.rb
@@ -13,6 +13,40 @@ class Vault < Formula
     bin.install "vault"
   end
 
+  plist_options manual: "vault server -dev"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+          </dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/vault</string>
+            <string>server</string>
+            <string>-dev</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/vault.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/vault.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
+
   test do
     system "#{bin}/vault --version"
   end
