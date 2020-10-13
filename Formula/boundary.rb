b8507bfc81a149bfae739ad2a36ce6e73d6d0f89
Kyle Penfound <kpenfound11@gmail.com>
2020-10-13 16:23:54 -0400

    add staged sha

--- Formula/boundary.rb
+++ Formula/boundary.rb
@@ -5,7 +5,7 @@ class Boundary < Formula
   url "https://releases.hashicorp.com/boundary/0.1.0/boundary_0.1.0_darwin_amd64.zip"
 
   version "0.1.0"
-  sha256 "TODO"
+  sha256 "fb8c64f98ac222ddd723c9c043a6afe5f50609080e549d82563c81d006c77271"
   bottle :unneeded
 
   conflicts_with "boundary"
