42dffd26a60aae619c520488e27b4bfe05120046
Kyle Penfound <kpenfound11@gmail.com>
2020-10-14 18:43:23 -0400

    release sha

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -5,16 +5,16 @@ class Waypoint < Formula
   url "https://releases.hashicorp.com/waypoint/0.1.0/waypoint_0.1.0_darwin_amd64.zip"
 
   version "0.1.0"
-  sha256 "TODO"
+  sha256 "b1adf2ef8844cbdee70b4fcbb6caba55839d433432346b08bb5a1d985bf7ef92"
   bottle :unneeded
 
-  conflicts_with "waypoint"
+  #conflicts_with "waypoint"
 
   def install
     bin.install "waypoint"
   end
 
   test do
-    system "#{bin}/waypoint --version"
+    system "#{bin}/waypoint version"
   end
 end
