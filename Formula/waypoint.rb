295ee21fe5430db0b044d708ddcb078d2af55ef4
Kyle Penfound <kpenfound11@gmail.com>
2020-10-15 12:44:46 -0400

    Bump Waypoint

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -2,10 +2,10 @@ class Waypoint < Formula
   desc "Waypoint"
   homepage "https://www.waypointproject.io/"
 
-  url "https://releases.hashicorp.com/waypoint/0.1.0/waypoint_0.1.0_darwin_amd64.zip"
+  url "https://releases.hashicorp.com/waypoint/0.1.1/waypoint_0.1.1_darwin_amd64.zip"
 
-  version "0.1.0"
-  sha256 "86be77f811024e966bb3f408ed343f3d5f65a0f19e10dad488911be558e0de67"
+  version "0.1.1"
+  sha256 "7d8c61da91794245ee46663ae608b4d3aeb5aa721822c3b6a6f0b09017cc33c5"
   bottle :unneeded
 
   #conflicts_with "waypoint"
