44d3a5c64f0d6bc68e5dceb55015c5f961bbc5ca
hashibot <hashibot-feedback@hashicorp.com>
2020-11-19 05:58:12 -0500

    Brew formula update for terraform-ls version v0.10.0

--- Formula/terraform-ls.rb
+++ Formula/terraform-ls.rb
@@ -2,17 +2,16 @@
 class TerraformLs < Formula
   desc "Terraform Language Server"
   homepage "https://github.com/hashicorp/terraform-ls"
-  version "0.9.0"
+  version "0.10.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://releases.hashicorp.com/terraform-ls/0.9.0/terraform-ls_0.9.0_darwin_amd64.zip"
-    sha256 "16425f73dd32274e7601ee1d6298671aac41bc7d0441d40e10f2aa3c94d26456"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://releases.hashicorp.com/terraform-ls/0.9.0/terraform-ls_0.9.0_linux_amd64.zip"
-      sha256 "fd99d920855af7637b96f84619d8a108dc4dd23d2d6a7e7bcdbb090a393718f1"
-    end
+    url "https://releases.hashicorp.com/terraform-ls/0.10.0/terraform-ls_0.10.0_darwin_amd64.zip"
+    sha256 "8871817576cfacf258d202ce4e85a940ff4908324a7a4f3d937125101aa58bcb"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://releases.hashicorp.com/terraform-ls/0.10.0/terraform-ls_0.10.0_linux_amd64.zip"
+    sha256 "889a0fd33036e8be59001b197e305fd9706d4e18db6dc2646ef634dbc9a46567"
   end
   
   conflicts_with "terraform-ls"
