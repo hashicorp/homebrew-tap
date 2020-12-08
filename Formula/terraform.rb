dddbdcab8675d555a5103a9ad8aa831dc9fd205e
hc-espd-packager <68665936+hc-espd-packager@users.noreply.github.com>
2020-12-08 21:20:51 +0000

    terraform 0.14.2

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -2,9 +2,9 @@ class Terraform < Formula
   desc "Terraform"
   homepage "https://www.terraform.io/"
 
-  url "https://releases.hashicorp.com/terraform/0.14.1/terraform_0.14.1_darwin_amd64.zip"
-  version "0.14.1"
-  sha256 "3077741547eaa8885aa0f8fb9ed160b6f069a55c8e8f908a316416a13c4407ca"
+  url "https://releases.hashicorp.com/terraform/0.14.2/terraform_0.14.2_darwin_amd64.zip"
+  version "0.14.2"
+  sha256 "31a7691f891f4e4c5d77b5eab9ecc86516df638a0b7cbde120c9e14bef68f7ac"
   bottle :unneeded
 
   conflicts_with "terraform"
