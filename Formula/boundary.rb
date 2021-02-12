619844fbc6a5218725ae771e458456ace4bafd1e
hc-espd-packager <68665936+hc-espd-packager@users.noreply.github.com>
2021-02-12 21:54:21 +0000

    boundary 0.1.6

--- Formula/boundary.rb
+++ Formula/boundary.rb
@@ -2,10 +2,10 @@ class Boundary < Formula
   desc "Boundary"
   homepage "https://www.boundaryproject.io/"
 
-  url "https://releases.hashicorp.com/boundary/0.1.5/boundary_0.1.5_darwin_amd64.zip"
+  url "https://releases.hashicorp.com/boundary/0.1.6/boundary_0.1.6_darwin_amd64.zip"
 
-  version "0.1.5"
-  sha256 "fff1ed548af5c56971ac6a5c37e7d1f717f24c46cf4e06b19c2b3c9275160586"
+  version "0.1.6"
+  sha256 "b2d4e777b144a3f44e49745a1fdbc6b77fb4b4ef45a2a9b16a0804cf5b88d1d9"
   bottle :unneeded
 
   # conflicts_with "boundary"
