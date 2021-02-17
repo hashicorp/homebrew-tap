e8c0fc9bf6738a2713294c19f67b4cc3174f0f0e
hc-espd-packager <68665936+hc-espd-packager@users.noreply.github.com>
2021-02-17 17:41:51 +0000

    waypoint 0.2.2

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -2,10 +2,10 @@ class Waypoint < Formula
   desc "Waypoint"
   homepage "https://www.waypointproject.io/"
 
-  url "https://releases.hashicorp.com/waypoint/0.2.1/waypoint_0.2.1_darwin_amd64.zip"
+  url "https://releases.hashicorp.com/waypoint/0.2.2/waypoint_0.2.2_darwin_amd64.zip"
 
-  version "0.2.1"
-  sha256 "75a0571a6770d8b8e03d8d1294f777f1c54a396a2940b62f89ecd8c66d32cdf3"
+  version "0.2.2"
+  sha256 "58de7a19291dd5ada36d0984825658de1ea4c8ce58cc81f76f3ab902a7b6c5b0"
   bottle :unneeded
 
   #conflicts_with "waypoint"
