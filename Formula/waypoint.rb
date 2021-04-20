ddb76561a18547c51ad1e0d9c87a52794fc029bb
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2021-04-20 18:59:57 +0000

    Bump waypoint 0.3.1

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -1,26 +1,26 @@
 class Waypoint < Formula
   desc "Waypoint"
   homepage "https://www.waypointproject.io/"
-  version "0.3.0"
+  version "0.3.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/waypoint/0.3.0/waypoint_0.3.0_darwin_amd64.zip"
-    sha256 "f8d245d1ef7cd3e6d4d8c03e3a8e863e6a8162648ef946a8834afb92d9651436"
+    url "https://releases.hashicorp.com/waypoint/0.3.1/waypoint_0.3.1_darwin_amd64.zip"
+    sha256 "e68492a84fd0b32d2d6bef69cb91682cde57a4988090e30aaf2bd8624aff8333"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/waypoint/0.3.0/waypoint_0.3.0_darwin_arm64.zip"
-    sha256 "471467a3d0414186c580265e5011ea9ba2e73be6363ea98c9d1615562f8b2b36"
+    url "https://releases.hashicorp.com/waypoint/0.3.1/waypoint_0.3.1_darwin_arm64.zip"
+    sha256 "371048f96cebb66419197c203b4e72905fb2e7f8303a76704e4329121da25768"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/waypoint/0.3.0/waypoint_0.3.0_linux_amd64.zip"
-    sha256 "80fa965a4c9f0c7b4e8fffa255a1823ff8df350523b336433debb17d79ad8d6d"
+    url "https://releases.hashicorp.com/waypoint/0.3.1/waypoint_0.3.1_linux_amd64.zip"
+    sha256 "caaad459343295a6f3b9354ff1ddca3b5e7e747506a33b82f84f3f14fc8d6ed4"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/waypoint/0.3.0/waypoint_0.3.0_linux_arm.zip"
-    sha256 "010f75266d99f164dc33c087aa71612d638f2ef15d389c6d1d854657dab30816"
+    url "https://releases.hashicorp.com/waypoint/0.3.1/waypoint_0.3.1_linux_arm.zip"
+    sha256 "ddece0143e5bde8b13a24ea00373953bb4fec30f02ae2b02921e6bf05d2b521f"
   end
 
   bottle :unneeded
