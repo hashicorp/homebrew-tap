93d599a3b938b80a9593c518f4c2854aa6dea3cc
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2021-04-26 20:18:55 +0000

    Bump terraform 0.15.1

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -1,11 +1,11 @@
 class Terraform < Formula
   desc "Terraform"
   homepage "https://www.terraform.io/"
-  version "0.15.0"
+  version "0.15.1"
 
   if OS.mac?
-    url "https://releases.hashicorp.com/terraform/0.15.0/terraform_0.15.0_darwin_amd64.zip"
-    sha256 "96537262e38008a421d329ce51c1bc2a1926f0b4e68270c92a81a8a42fa2c513"
+    url "https://releases.hashicorp.com/terraform/0.15.1/terraform_0.15.1_darwin_amd64.zip"
+    sha256 "dd7220e6a76e4c9555576c0500bea94c6a5cb65f286b3e74e8ea7cc34bbce5be"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,18 +20,18 @@ class Terraform < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/terraform/0.15.0/terraform_0.15.0_linux_amd64.zip"
-    sha256 "69c5db7bd6d4a5d3dd060678e5c3d9442e32610ed05879b4325e6aa4807d0529"
+    url "https://releases.hashicorp.com/terraform/0.15.1/terraform_0.15.1_linux_amd64.zip"
+    sha256 "1ff798791abf518fb0b5d9958ec8327b7213f1c91fb5235923e91cc96c59ef2c"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/terraform/0.15.0/terraform_0.15.0_linux_arm.zip"
-    sha256 "f9b034762d0acdb04b4d7a095c1c6a4088e6e064cc510a6d6e2479e957406204"
+    url "https://releases.hashicorp.com/terraform/0.15.1/terraform_0.15.1_linux_arm.zip"
+    sha256 "e39b377edf4cf029ecd8ae38c248a3f6a3921d8bfaa1ca0dcc5c960c1b4945de"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/terraform/0.15.0/terraform_0.15.0_linux_arm64.zip"
-    sha256 "a83897c82bbfc01a390fbd9acd3cd0cc91f179705385b6ae276a0d8de98a8cba"
+    url "https://releases.hashicorp.com/terraform/0.15.1/terraform_0.15.1_linux_arm64.zip"
+    sha256 "39aa72b66b5802a2909a17ed14ffe7bcf3b6466a91710989c4e6cb854d594538"
   end
 
   bottle :unneeded
