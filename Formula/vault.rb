5928dbc55934226ac8a99ad243b1b8e3d3f3d03e
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2021-05-20 18:44:43 +0000

    Bump vault 1.7.2

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -1,11 +1,11 @@
 class Vault < Formula
   desc "Vault"
   homepage "https://www.vaultproject.io"
-  version "1.7.1"
+  version "1.7.2"
 
   if OS.mac?
-    url "https://releases.hashicorp.com/vault/1.7.1/vault_1.7.1_darwin_amd64.zip"
-    sha256 "f08f61231a31875334204ac94e726cf032e284ee95b096ab475b3699b9fb3f90"
+    url "https://releases.hashicorp.com/vault/1.7.2/vault_1.7.2_darwin_amd64.zip"
+    sha256 "7d37e12ccb9497a9e403d562d837b2ea7b99dbe919e6956ade1e2e410a54f573"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,18 +20,18 @@ class Vault < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault/1.7.1/vault_1.7.1_linux_amd64.zip"
-    sha256 "61795d2deeffddee0b556869097c997c5afccbde556d3a103ec273cc24523808"
+    url "https://releases.hashicorp.com/vault/1.7.2/vault_1.7.2_linux_amd64.zip"
+    sha256 "5ee6bb8119b55c27cd3864c982177714a0a4a3813927ccafdb262e78e4bb67bc"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault/1.7.1/vault_1.7.1_linux_arm.zip"
-    sha256 "f334e35a6897513f94dae056c5609d8837e512378659cfb262b5a5982376437b"
+    url "https://releases.hashicorp.com/vault/1.7.2/vault_1.7.2_linux_arm.zip"
+    sha256 "38782995fecb298f4a6c55e5568a7ea54a8516a7ea4ebaf25bb7caf0f6111c4a"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault/1.7.1/vault_1.7.1_linux_arm64.zip"
-    sha256 "58dd1744979c3461e92d5f3e02d94056446286e555cc96888c627d837cad99de"
+    url "https://releases.hashicorp.com/vault/1.7.2/vault_1.7.2_linux_arm64.zip"
+    sha256 "2bb9d49b253893ffa2149ee85ce2f2bc72360a2c14ac8775155f34c572344533"
   end
 
   bottle :unneeded
