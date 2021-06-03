91ce628bafe4afac6a4a6b7b4dbedb0c4991f001
Kyle Penfound <kpenfound11@gmail.com>
2021-06-03 14:48:38 -0400

    Bump boundary-desktop 1.0.1

--- Casks/hashicorp-boundary-desktop.rb
+++ Casks/hashicorp-boundary-desktop.rb
@@ -1,6 +1,6 @@
 cask "hashicorp-boundary-desktop" do
-  version "1.0.0"
-  sha256 "c84f443f5e19af494135bb5e9998f27d4b52c213ddf57545f0be9027010b411d"
+  version "1.0.1"
+  sha256 "6bac4b7d28b563ef80966b070fc4ecc610a38c05a30202307c00cf26355ae600"
 
   url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_amd64.dmg"
   name "Boundary Desktop"
