eff3bc9241a3c206ece02b53efa9d35d7964cf90
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2021-09-22 21:40:27 +0000

    Bump consul-template 0.27.1

--- Formula/consul-template.rb
+++ Formula/consul-template.rb
@@ -1,11 +1,11 @@
 class ConsulTemplate < Formula
   desc "Consul Template"
   homepage "https://github.com/hashicorp/consul-template"
-  version "0.27.0"
+  version "0.27.1"
 
   if OS.mac?
-    url "https://releases.hashicorp.com/consul-template/0.27.0/consul-template_0.27.0_darwin_amd64.zip"
-    sha256 "b015cb821c6e261248bef36fa9b00fab356ffa7365c566dcaa9a8fd8e6e42a1e"
+    url "https://releases.hashicorp.com/consul-template/0.27.1/consul-template_0.27.1_darwin_amd64.zip"
+    sha256 "95d832271ba49de4e647cf9bf5cb7de1cf29bfd47be26dba30d456dbda7690af"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,13 +20,13 @@ class ConsulTemplate < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/consul-template/0.27.0/consul-template_0.27.0_linux_amd64.zip"
-    sha256 "0d319977885e0f44562cc5f78e225d8431499cc3a95cd1b3fe560df8556bf64a"
+    url "https://releases.hashicorp.com/consul-template/0.27.1/consul-template_0.27.1_linux_amd64.zip"
+    sha256 "db340b7555105d004caf8fad10e1d53ee0bd2320572c4560387279e92490f807"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul-template/0.27.0/consul-template_0.27.0_linux_arm.zip"
-    sha256 ""
+    url "https://releases.hashicorp.com/consul-template/0.27.1/consul-template_0.27.1_linux_arm.zip"
+    sha256 "ee6d18c70664dbb0eb3f1061188112aaef7b0cf972e8a409c8209ffd488e8567"
   end
 
   bottle :unneeded
