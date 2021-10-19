995721a140fa27e1e0270ba1e4673ec93d9f60eb
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2021-10-19 15:28:50 +0000

    Bump packer 1.7.7

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -1,31 +1,31 @@
 class Packer < Formula
   desc "Packer"
   homepage "https://www.packer.io/"
-  version "1.7.6"
+  version "1.7.7"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/packer/1.7.6/packer_1.7.6_darwin_amd64.zip"
-    sha256 "dd947f2ca83e48614ea0b858455ce4dda61151c84d012af9d33179f2019cc451"
+    url "https://releases.hashicorp.com/packer/1.7.7/packer_1.7.7_darwin_amd64.zip"
+    sha256 "618f20f3931a6ff5e8d7412290121b90537554af5ebbbae6c6a77ad50bee2ef6"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/packer/1.7.6/packer_1.7.6_darwin_arm64.zip"
-    sha256 "3e9214dfae97183b0f6d24651b3a1b19e37547f6815a3266b3b02bad16f7c9c9"
+    url "https://releases.hashicorp.com/packer/1.7.7/packer_1.7.7_darwin_arm64.zip"
+    sha256 "903f92543b2d7e1306d58159b75b96af693e1a061710c06937d7782d17d4bfb1"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/packer/1.7.6/packer_1.7.6_linux_amd64.zip"
-    sha256 "d2717af4d728cd9034c7a3bb2c6eac384d772bae78d856413b14742a0ca28a1c"
+    url "https://releases.hashicorp.com/packer/1.7.7/packer_1.7.7_linux_amd64.zip"
+    sha256 "8513c3679d51141c39da3d95c691fcfc4b2ccc20e96ac5244b58b98899d6fe54"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/packer/1.7.6/packer_1.7.6_linux_arm.zip"
-    sha256 "1f546ccafd81860d7f53744f696db295981fa6502438705ccc658d008374ba0e"
+    url "https://releases.hashicorp.com/packer/1.7.7/packer_1.7.7_linux_arm.zip"
+    sha256 "27aefc9f6235817163d31fec582b84dc676431049448a584b371b9307b822433"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/packer/1.7.6/packer_1.7.6_linux_arm64.zip"
-    sha256 "903f428556a75bcbda73505ebbc577e4614455d9afb56b947270aa24f9bb3c4b"
+    url "https://releases.hashicorp.com/packer/1.7.7/packer_1.7.7_linux_arm64.zip"
+    sha256 "c44d4f4c6776154a95a700c4b8caefb07c96652deb4ca6b265d19d767b31faeb"
   end
 
   bottle :unneeded
