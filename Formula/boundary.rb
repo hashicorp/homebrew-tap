ff5ecaedd423b49235792eaf3360fea86cb3ef0c
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2021-11-17 22:52:47 +0000

    Bump boundary 0.7.0

--- Formula/boundary.rb
+++ Formula/boundary.rb
@@ -1,11 +1,11 @@
 class Boundary < Formula
   desc "Boundary"
   homepage "https://www.boundaryproject.io/"
-  version "0.6.2"
+  version "0.7.0"
 
   if OS.mac?
-    url "https://releases.hashicorp.com/boundary/0.6.2/boundary_0.6.2_darwin_amd64.zip"
-    sha256 "0ca0c4ae90fa68ae38ec930a97965ca692bfcb2a0e039e58d60edc5d5386778d"
+    url "https://releases.hashicorp.com/boundary/0.7.0/boundary_0.7.0_darwin_amd64.zip"
+    sha256 "6a7a3f0166b274471059ac6a22e1fff03cc90274443e98e6d6d7b0f858e2c033"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,18 +20,18 @@ class Boundary < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/boundary/0.6.2/boundary_0.6.2_linux_amd64.zip"
-    sha256 "42a7c865c5970e311a9222629bbbdeaec6e7ea315f7e843793dd3cc1b84db240"
+    url "https://releases.hashicorp.com/boundary/0.7.0/boundary_0.7.0_linux_amd64.zip"
+    sha256 "3ba74cc32ce816fe5ac5fecdd71c760e02e87c2f85e5b0d58db81113dab4b635"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/boundary/0.6.2/boundary_0.6.2_linux_arm.zip"
-    sha256 "335fd9d9f73aa8def51a3f78910555e0d3504a7e5b3ef7c8b48356f7620519ee"
+    url "https://releases.hashicorp.com/boundary/0.7.0/boundary_0.7.0_linux_arm.zip"
+    sha256 "88e942addb10431e58a1483298931d9df3672a1ce0b43682f39204962e4d90ff"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/boundary/0.6.2/boundary_0.6.2_linux_arm64.zip"
-    sha256 "6979dfad5bd7e12503c75858a82603adb1ff8c5a5a902a101f81a794f7052386"
+    url "https://releases.hashicorp.com/boundary/0.7.0/boundary_0.7.0_linux_arm64.zip"
+    sha256 "a5d7e08f8f7680f88b882ae7e39798df21f043374ab56a3970555c0ea81ff1c2"
   end
 
   conflicts_with "boundary"
