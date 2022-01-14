93dc4645047cd9d3ca8fa24732cfca771c32481f
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-01-14 05:38:07 +0000

    Bump sentinel 0.18.5

--- Formula/sentinel.rb
+++ Formula/sentinel.rb
@@ -1,11 +1,11 @@
 class Sentinel < Formula
   desc ""
   homepage "https://docs.hashicorp.com/sentinel"
-  version "0.18.4"
+  version "0.18.5"
 
   if OS.mac?
-    url "https://releases.hashicorp.com/sentinel/0.18.4/sentinel_0.18.4_darwin_amd64.zip"
-    sha256 "7ce71e5850e46e71814ba92d0ae6596bb0b89d184b7b8a59e9921e0d4d4bd72f"
+    url "https://releases.hashicorp.com/sentinel/0.18.5/sentinel_0.18.5_darwin_amd64.zip"
+    sha256 "ebafbc6cfe93b13bb090be2c623f611e11ab360f95136156ce63d54ca6245bd9"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,17 +20,17 @@ class Sentinel < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/sentinel/0.18.4/sentinel_0.18.4_linux_amd64.zip"
-    sha256 "a2969317b38abc201aded53592cf931c150455aeb0061116f3459880c9d9a41a"
+    url "https://releases.hashicorp.com/sentinel/0.18.5/sentinel_0.18.5_linux_amd64.zip"
+    sha256 "0ee6baddf312a418685e4a48fb9a1ea3ad956358b9d328b3b7b198af6c4b9f41"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/sentinel/0.18.4/sentinel_0.18.4_linux_arm.zip"
-    sha256 "3484a11f83531bf3c62b670b8b6df1203b3913333dfca3e67e223b508c53423e"
+    url "https://releases.hashicorp.com/sentinel/0.18.5/sentinel_0.18.5_linux_arm.zip"
+    sha256 "e698e1e1b195f27cfb3a7715b09fbc8e6a24ab2f2d69ed39dacf95e2ce4255af"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/sentinel/0.18.4/sentinel_0.18.4_linux_arm64.zip"
+    url "https://releases.hashicorp.com/sentinel/0.18.5/sentinel_0.18.5_linux_arm64.zip"
     sha256 ""
   end
 
