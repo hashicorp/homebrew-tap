269ff04fde263bd0f0924462000e59a98acca396
Kyle Penfound <kpenfound11@gmail.com>
2022-03-02 11:51:10 -0500

    sanitize productname

--- util/lambda_trigger/releases.go
+++ util/lambda_trigger/releases.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 
 	hb "github.com/gulducat/hashi-bin/types"
 )
@@ -10,7 +11,8 @@ import (
 const ReleasesURL = "https://hc-releases-prod.s3.amazonaws.com"
 
 func getLatestVersion(productName string) (*hb.Version, error) {
-	url := fmt.Sprintf("%s/%s/%s", ReleasesURL, productName, "index.json")
+	safeProduct := url.PathEscape(productName)
+	url := fmt.Sprintf("%s/%s/%s", ReleasesURL, safeProduct, "index.json")
 	product, err := hb.NewProduct(url)
 	if err != nil {
 		return &hb.Version{}, err
