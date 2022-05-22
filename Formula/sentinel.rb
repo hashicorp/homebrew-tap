c0980388753b48fda3c4e0686c41d6aa4a1cadd6
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-05-22 23:17:09 +0000

    Bump sentinel 0.18.10

--- Formula/sentinel.rb
+++ Formula/sentinel.rb
@@ -1,11 +1,11 @@
 class Sentinel < Formula
   desc ""
   homepage "https://docs.hashicorp.com/sentinel"
-  version "0.18.9"
+  version "0.18.10"
 
   if OS.mac?
-    url "https://releases.hashicorp.com/sentinel/0.18.9/sentinel_0.18.9_darwin_amd64.zip"
-    sha256 "22e896d631283e2dbde09b8b4e19799c4d6ecc8a234e73c96c2aba07f685f95d"
+    url "https://releases.hashicorp.com/sentinel/0.18.10/sentinel_0.18.10_darwin_amd64.zip"
+    sha256 "c79d982cf24bbce38bcdbf7deb752ff629d76bba33c9f2b04f48be957113885d"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,18 +20,18 @@ class Sentinel < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/sentinel/0.18.9/sentinel_0.18.9_linux_amd64.zip"
-    sha256 "33e76b1a2e47d52a2fd4d2e2c339f0a397675ab0bda44d1605a616b6cf50cac6"
+    url "https://releases.hashicorp.com/sentinel/0.18.10/sentinel_0.18.10_linux_amd64.zip"
+    sha256 "31fb780c1577c0ee12f2f2824cd33663a7e17671f5667293f72f33a23448d7ba"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/sentinel/0.18.9/sentinel_0.18.9_linux_arm.zip"
-    sha256 "46e1c2b531759d2087bce430a983d21a2e1b1ced8da638d8ab2b9b60a869ba01"
+    url "https://releases.hashicorp.com/sentinel/0.18.10/sentinel_0.18.10_linux_arm.zip"
+    sha256 "dd7406a37c90528bf0bb3d4052df6c9d8c37dd7497a788e3e6ee4b80b6a240ba"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/sentinel/0.18.9/sentinel_0.18.9_linux_arm64.zip"
-    sha256 "3fd0bbd6913d7ce1b16320f2b1a28a61125b31bf3a1d94002d21842f76fe6f29"
+    url "https://releases.hashicorp.com/sentinel/0.18.10/sentinel_0.18.10_linux_arm64.zip"
+    sha256 "a828598dde8f662cbb788eca50b16bc0add5d3982524baa92fb5efe54b9cf500"
   end
 
   conflicts_with "sentinel"
