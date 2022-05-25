49f0f8373c03916581b7f17bddf589332a04447b
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-05-25 16:09:00 +0000

    Bump consul-terraform-sync 0.6.0

--- Formula/consul-terraform-sync.rb
+++ Formula/consul-terraform-sync.rb
@@ -1,11 +1,11 @@
 class ConsulTerraformSync < Formula
   desc "Consul Terraform Sync"
   homepage "https://github.com/hashicorp/consul-terraform-sync"
-  version "0.5.2"
+  version "0.6.0"
 
   if OS.mac?
-    url "https://releases.hashicorp.com/consul-terraform-sync/0.5.2/consul-terraform-sync_0.5.2_darwin_amd64.zip"
-    sha256 "b63737821c735dd59efa006d4ff90cb3c89505078b91c3ee2dcd4bab1f7e8833"
+    url "https://releases.hashicorp.com/consul-terraform-sync/0.6.0/consul-terraform-sync_0.6.0_darwin_amd64.zip"
+    sha256 "bff81465e8c9426fbf3e4192c9544394bda622b2641efa2ed462750d0ec7dd1b"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,13 +20,13 @@ class ConsulTerraformSync < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/consul-terraform-sync/0.5.2/consul-terraform-sync_0.5.2_linux_amd64.zip"
-    sha256 "3bfe5cb444515a1aa7a6362af956fc656d3063b621f5dffb63a97614059122e4"
+    url "https://releases.hashicorp.com/consul-terraform-sync/0.6.0/consul-terraform-sync_0.6.0_linux_amd64.zip"
+    sha256 "29d70b655e2dce3f92afdd797eabcb5b14a77487739d31daa0ce8b8055f01478"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul-terraform-sync/0.5.2/consul-terraform-sync_0.5.2_linux_arm.zip"
-    sha256 "45c0d7b7e9dbb61a6bdd10cdb81b2e23a44db22960af7ccfc94c86222abb5272"
+    url "https://releases.hashicorp.com/consul-terraform-sync/0.6.0/consul-terraform-sync_0.6.0_linux_arm.zip"
+    sha256 "956bb2076fa02e3b755b2dc231ac6eca7c34961ce818a5a7e34cf68804924b21"
   end
 
   conflicts_with "consul-terraform-sync"
