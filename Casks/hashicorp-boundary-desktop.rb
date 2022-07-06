4dd30e11ea6af73d42d1f946e156197090a03b58
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-07-06 21:39:12 +0000

    Bump boundary-desktop 1.4.5

--- Casks/hashicorp-boundary-desktop.rb
+++ Casks/hashicorp-boundary-desktop.rb
@@ -1,6 +1,6 @@
 cask "hashicorp-boundary-desktop" do
-  version "1.4.4"
-  sha256 "2980a6b6030e3dbd856457a1a72324e813533ced4b8d54d085e189b5a0ba0e0f"
+  version "1.4.5"
+  sha256 "92329c3841a53171f00feda0be7a261958f76881fd19b39400c894f0e9133ba1"
 
   url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_amd64.dmg"
   name "Boundary Desktop"
