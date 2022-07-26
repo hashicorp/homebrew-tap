d7a9746332e03ce04096389223fe1c9f7442c19d
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-07-26 23:21:21 +0000

    Bump consul-k8s 0.46.1

--- Formula/consul-k8s.rb
+++ Formula/consul-k8s.rb
@@ -1,11 +1,11 @@
 class ConsulK8s < Formula
   desc "Consul K8s"
   homepage "https://github.com/hashicorp/consul-k8s"
-  version "0.46.0"
+  version "0.46.1"
 
   if OS.mac?
-    url "https://releases.hashicorp.com/consul-k8s/0.46.0/consul-k8s_0.46.0_darwin_amd64.zip"
-    sha256 "9df3a9dc2a15a6a7bbc30eb03e70d800f4aae5e6b566442b1e9a708d562331a2"
+    url "https://releases.hashicorp.com/consul-k8s/0.46.1/consul-k8s_0.46.1_darwin_amd64.zip"
+    sha256 "7e8620a8746c1e3216f0c79f10096fef53db0dde4229d4176cb4a0edadaa2054"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,18 +20,18 @@ class ConsulK8s < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/consul-k8s/0.46.0/consul-k8s_0.46.0_linux_amd64.zip"
-    sha256 "1a4bcbea49f700d513984de34455256330a955889319b265aba61d0e8c08bb7b"
+    url "https://releases.hashicorp.com/consul-k8s/0.46.1/consul-k8s_0.46.1_linux_amd64.zip"
+    sha256 "5e411a28667348061cc00a9dbcc5d4ab2f60b5d99c7f33c44d6a827a8629e792"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul-k8s/0.46.0/consul-k8s_0.46.0_linux_arm.zip"
-    sha256 "ed3f8cf737e0de7e1b88aea9f707047d260e41a79ea0584840fee64c610a1e38"
+    url "https://releases.hashicorp.com/consul-k8s/0.46.1/consul-k8s_0.46.1_linux_arm.zip"
+    sha256 "f00ff66ceb1e12c26f0fbe378d3548996260d03c81cfbbea6ae53eae2e65d267"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul-k8s/0.46.0/consul-k8s_0.46.0_linux_arm64.zip"
-    sha256 "6ef31d4bcb5823cf9472035b8bd58cd459943aff47bd30d446ae870b71cce038"
+    url "https://releases.hashicorp.com/consul-k8s/0.46.1/consul-k8s_0.46.1_linux_arm64.zip"
+    sha256 "5dc4654b6c1d26c300de678b6c2e0b282fa09ed847cc0f89a1c149d7c7367bec"
   end
 
   conflicts_with "consul-k8s"
