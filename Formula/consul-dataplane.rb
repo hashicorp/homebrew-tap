4dc4e3cb8d98fd04514627c7458b63d906702dd9
Paul Glass <pglass@hashicorp.com>
2022-09-30 14:17:29 -0500

    Correct consul-dataplane version to 1.0.0-beta1

--- Formula/consul-dataplane.rb
+++ Formula/consul-dataplane.rb
@@ -1,33 +1,33 @@
 class ConsulDataplane < Formula
   desc "Consul Dataplane"
   homepage "https://github.com/hashicorp/consul-dataplane"
-  version "0.1.0-beta"
+  version "1.0.0-beta1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/consul-dataplane/0.1.0-beta/consul-dataplane_0.1.0-beta_darwin_amd64.zip"
+    url "https://releases.hashicorp.com/consul-dataplane/1.0.0-beta1/consul-dataplane_1.0.0-beta1_darwin_amd64.zip"
     sha256 ""
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/consul-dataplane/0.1.0-beta/consul-dataplane_0.1.0-beta_darwin_arm64.zip"
+    url "https://releases.hashicorp.com/consul-dataplane/1.0.0-beta1/consul-dataplane_1.0.0-beta1_darwin_arm64.zip"
     sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/consul-dataplane/0.1.0-beta/consul-dataplane_0.1.0-beta_linux_amd64.zip"
+    url "https://releases.hashicorp.com/consul-dataplane/1.0.0-beta1/consul-dataplane_1.0.0-beta1_linux_amd64.zip"
     sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul-dataplane/0.1.0-beta/consul-dataplane_0.1.0-beta_linux_arm.zip"
+    url "https://releases.hashicorp.com/consul-dataplane/1.0.0-beta1/consul-dataplane_1.0.0-beta1_linux_arm.zip"
     sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul-dataplane/0.1.0-beta/consul-dataplane_0.1.0-beta_linux_arm64.zip"
+    url "https://releases.hashicorp.com/consul-dataplane/1.0.0-beta1/consul-dataplane_1.0.0-beta1_linux_arm64.zip"
     sha256 ""
   end
-  
+
   depends_on "envoy" => :recommended
 
   conflicts_with "consul-dataplane"
