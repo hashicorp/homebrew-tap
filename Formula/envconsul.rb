5d04533baa5a6c925b35e11bcc83f99514fc5516
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-10-03 23:33:58 +0000

    Bump envconsul 0.13.1

--- Formula/envconsul.rb
+++ Formula/envconsul.rb
@@ -1,37 +1,31 @@
 class Envconsul < Formula
   desc "Env Consul"
   homepage "https://github.com/hashicorp/envconsul"
-  version "0.13.0"
+  version "0.13.1"
 
-  if OS.mac?
-    url "https://releases.hashicorp.com/envconsul/0.13.0/envconsul_0.13.0_darwin_amd64.zip"
-    sha256 "766b41be96f0829ed15de3f4e6c5e2b8bc89709794ad174f36524522282acbee"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://releases.hashicorp.com/envconsul/0.13.1/envconsul_0.13.1_darwin_amd64.zip"
+    sha256 "c1a6bfa8158d9841f6e6ff7788d8adb8ca9f5b804d9408688625f8434fe9c5b7"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    def caveats
-      <<~EOS
-        The darwin_arm64 architecture is not supported for this product
-        at this time, however we do plan to support this in the future. The
-        darwin_amd64 binary has been installed and may work in
-        compatibility mode, but it is not fully supported.
-      EOS
-    end
+    url "https://releases.hashicorp.com/envconsul/0.13.1/envconsul_0.13.1_darwin_arm64.zip"
+    sha256 "85d8349f977c945fc4e90799d3af4dbacd3705b549e5ddcafe9767cf703f9e24"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/envconsul/0.13.0/envconsul_0.13.0_linux_amd64.zip"
-    sha256 "1db7fa993d4e6182eab10d17210e36b94848799ed4dd75582bf642260af3db53"
+    url "https://releases.hashicorp.com/envconsul/0.13.1/envconsul_0.13.1_linux_amd64.zip"
+    sha256 "a28c5ebfa1bdd7c5317dcb17bb7693b0813bd44d1d477dfa6149f48052267905"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/envconsul/0.13.0/envconsul_0.13.0_linux_arm.zip"
-    sha256 "4b368b5b137562f8cf8eba74cb04d639bbbfd5a307cad9daabf23aa51dba6d24"
+    url "https://releases.hashicorp.com/envconsul/0.13.1/envconsul_0.13.1_linux_arm.zip"
+    sha256 "bd2fe1430d32cf634c4dfa5d5e0e15e40b8b8d8631f0f60ca76e28e7c1c45835"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/envconsul/0.13.0/envconsul_0.13.0_linux_arm64.zip"
-    sha256 "6c8e489076ec1180018c1ac2630019657cb2cec298d025065e45d5d94105742d"
+    url "https://releases.hashicorp.com/envconsul/0.13.1/envconsul_0.13.1_linux_arm64.zip"
+    sha256 "87867b87accaacc5dea89c8123bf2923767fa584ca29ba46103a47519dcfdeb0"
   end
 
   conflicts_with "envconsul"
