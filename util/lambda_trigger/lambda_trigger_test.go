2164fdf4267eb6c1412ac444f3afe9c790da81ae
Michele Degges <mdeggies@gmail.com>
2022-10-04 14:20:46 -0700

    Add cask tests; run tests in CI

--- util/lambda_trigger/lambda_trigger_test.go
+++ util/lambda_trigger/lambda_trigger_test.go
@@ -7,35 +7,51 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const test_oss_product = "nomad"
-const test_ent_product = "nomad-enterprise"
-const test_oss_version = "1.3.5"
-const test_ent_version = "1.3.5+ent"
+const test_oss_product_formula = "nomad"
+const test_oss_product_cask = "boundary-desktop"
+const test_ent_product_formula = "nomad-enterprise"
+const test_oss_version_formula = "1.3.5"
+const test_ent_version_formula = "1.3.5+ent"
+const test_oss_version_cask = "1.5.0"
 
 func TestOSSGetFormulaVersion(t *testing.T) {
-	gotLatest, err := getFormulaVersion(test_oss_product)
+	gotLatest, err := getFormulaVersion(test_oss_product_formula)
 
 	require.Nil(t, err)
-	assert.Equal(t, test_oss_version, gotLatest)
+	assert.Equal(t, test_oss_version_formula, gotLatest)
 }
 
 func TestOSSGetLatestVersion(t *testing.T) {
-	gotLatest, err := getLatestVersion(test_oss_product)
+	gotLatest, err := getLatestVersion(test_oss_product_formula)
 
 	require.Nil(t, err)
-	assert.Equal(t, test_oss_version, *gotLatest)
+	assert.Equal(t, test_oss_version_formula, *gotLatest)
+}
+
+func TestOSSGetCaskVersion(t *testing.T) {
+	gotLatest, err := getCaskVersion(test_oss_product_cask)
+
+	require.Nil(t, err)
+	assert.Equal(t, test_oss_version_cask, gotLatest)
+}
+
+func TestOSSGetLatestCaskVersion(t *testing.T) {
+	gotLatest, err := getLatestVersion(test_oss_product_cask)
+
+	require.Nil(t, err)
+	assert.Equal(t, test_oss_version_cask, *gotLatest)
 }
 
 func TestENTGetFormulaVersion(t *testing.T) {
-	gotLatest, err := getFormulaVersion(test_ent_product)
+	gotLatest, err := getFormulaVersion(test_ent_product_formula)
 
 	require.Nil(t, err)
-	assert.Equal(t, test_ent_version, gotLatest)
+	assert.Equal(t, test_ent_version_formula, gotLatest)
 }
 
 func TestENTGetLatestVersion(t *testing.T) {
-	gotLatest, err := getLatestVersion(test_ent_product)
+	gotLatest, err := getLatestVersion(test_ent_product_formula)
 
 	require.Nil(t, err)
-	assert.Equal(t, test_ent_version, *gotLatest)
+	assert.Equal(t, test_ent_version_formula, *gotLatest)
 }
