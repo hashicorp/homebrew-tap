29c1eb541e879d28f6db7f49a96ea0f3cc23b5da
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-10-21 08:20:20 +0000

    Bump levant 0.3.2

--- Formula/levant.rb
+++ Formula/levant.rb
@@ -1,11 +1,11 @@
 class Levant < Formula
   desc "Levant"
   homepage "https://github.com/hashicorp/levant"
-  version "0.3.1"
+  version "0.3.2"
 
   if OS.mac?
-    url "https://releases.hashicorp.com/levant/0.3.1/levant_0.3.1_darwin_amd64.zip"
-    sha256 "e151a7bc01d5861387af9de6c97625e0a96b61d087839f34c057f5a9b6233135"
+    url "https://releases.hashicorp.com/levant/0.3.2/levant_0.3.2_darwin_amd64.zip"
+    sha256 "da28950551f30198401f6ca7020d6daaf91f9349174fc805297cde73eff1b81d"
   end
 
   if OS.mac? && Hardware::CPU.arm?
@@ -20,18 +20,18 @@ class Levant < Formula
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/levant/0.3.1/levant_0.3.1_linux_amd64.zip"
-    sha256 "01b4a15a1eb5d6c69178ff383a457a6251658224dd9537fe1ea9f24e07044f76"
+    url "https://releases.hashicorp.com/levant/0.3.2/levant_0.3.2_linux_amd64.zip"
+    sha256 "1e06180f8d8327e721e2ad4c9637d1f391034a22709473b8ed8ee19f1cad1570"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/levant/0.3.1/levant_0.3.1_linux_arm.zip"
-    sha256 "9b7176f351915e0dbc030ab8347c09ee4f10f8b4f4f807637ef8fc23e42f30c2"
+    url "https://releases.hashicorp.com/levant/0.3.2/levant_0.3.2_linux_arm.zip"
+    sha256 "3c511d66e9018fdfefb179f5558f1639aa25f2a3a3376b7033f10a505f3c09a2"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/levant/0.3.1/levant_0.3.1_linux_arm64.zip"
-    sha256 "a30190ba4aa5fd7e6b2ef8cd4e0cd949349736f00d215c8c28137c85a0f1ae3d"
+    url "https://releases.hashicorp.com/levant/0.3.2/levant_0.3.2_linux_arm64.zip"
+    sha256 "8cb3c3c10b5415704964b437d85a2f26091bf25e8575df76403874eee78934b1"
   end
 
   conflicts_with "levant"
