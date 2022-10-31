897f4d77724a1f3c64f829d80e12c0ce31abc232
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-10-31 03:36:54 +0000

    Bump sentinel 0.18.13

--- Formula/sentinel.rb
+++ Formula/sentinel.rb
@@ -1,37 +1,31 @@
 class Sentinel < Formula
   desc ""
   homepage "https://docs.hashicorp.com/sentinel"
-  version "0.18.12"
+  version "0.18.13"
 
-  if OS.mac?
-    url "https://releases.hashicorp.com/sentinel/0.18.12/sentinel_0.18.12_darwin_amd64.zip"
-    sha256 "83332a987aa9c9bb8be5e6bc13344eec60b8d4060067b8ad4c67aa65fca92de0"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://releases.hashicorp.com/sentinel/0.18.13/sentinel_0.18.13_darwin_amd64.zip"
+    sha256 "97583fde45562ab2f6cb31029c86a080a4eefe86d99d8229132536441540df0c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    def caveats
-      <<~EOS
-        The darwin_arm64 architecture is not supported for this product
-        at this time, however we do plan to support this in the future. The
-        darwin_amd64 binary has been installed and may work in
-        compatibility mode, but it is not fully supported.
-      EOS
-    end
+    url "https://releases.hashicorp.com/sentinel/0.18.13/sentinel_0.18.13_darwin_arm64.zip"
+    sha256 "604278234102613c1d9e2fe57235146d6f29b68cfbd6eb099e07f83f941def7e"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/sentinel/0.18.12/sentinel_0.18.12_linux_amd64.zip"
-    sha256 "c205ca51850bdd6dd00d076eeb61fffd2b78cf6501b69261a11eb6f9a099a769"
+    url "https://releases.hashicorp.com/sentinel/0.18.13/sentinel_0.18.13_linux_amd64.zip"
+    sha256 "0584f91f3f7e016086ed710144f415c4dfa2e37b1d68297f7423aed38ff308cf"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/sentinel/0.18.12/sentinel_0.18.12_linux_arm.zip"
-    sha256 "f361b386eea299b15b11c95e10837c421b863dd3ef47ed885a9a6a2988e77e10"
+    url "https://releases.hashicorp.com/sentinel/0.18.13/sentinel_0.18.13_linux_arm.zip"
+    sha256 "51532ababc1661103f8e0af6e286b13c8830df5ecffb1e76abbcbaa5c15e2ba9"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/sentinel/0.18.12/sentinel_0.18.12_linux_arm64.zip"
-    sha256 "07f13731d9c0a38082ba31935015f8f33ac44931aebcd65dde5e78e1f2fee173"
+    url "https://releases.hashicorp.com/sentinel/0.18.13/sentinel_0.18.13_linux_arm64.zip"
+    sha256 "3eec73604e1785c6dcf44172c6e2f6cb5e4c2dffe13df371008a4b909a971a2d"
   end
 
   conflicts_with "sentinel"
