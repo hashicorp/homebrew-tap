1d7907e6ef33b3ba08762d0d585c23e8f22c4e26
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-12-08 17:24:05 +0000

    Bump waypoint 0.10.4

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -1,31 +1,31 @@
 class Waypoint < Formula
   desc "Waypoint"
   homepage "https://www.waypointproject.io/"
-  version "0.10.3"
+  version "0.10.4"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/waypoint/0.10.3/waypoint_0.10.3_darwin_amd64.zip"
-    sha256 "91e00214923237c6d8d29734d49c66deabe27295193d17582f2a54e28ebd806d"
+    url "https://releases.hashicorp.com/waypoint/0.10.4/waypoint_0.10.4_darwin_amd64.zip"
+    sha256 "e360bd8d041c3da4f5f3ba647103fcd6ce1dac56412140a858c1ac2bd9156bb9"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/waypoint/0.10.3/waypoint_0.10.3_darwin_arm64.zip"
-    sha256 "3d7de189a04ebe6a6d5004086b343b0cfe94b8bcaac67490449bb350cb3e8a1c"
+    url "https://releases.hashicorp.com/waypoint/0.10.4/waypoint_0.10.4_darwin_arm64.zip"
+    sha256 "2a9a4c7df99a52a4a5abb7c81e62d08f7c74cc13180c575d3df960bc58747b53"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/waypoint/0.10.3/waypoint_0.10.3_linux_amd64.zip"
-    sha256 "2a97de706246b3e06147af49fe2dd6226162d9f0a2f966931eee16ecbd6eec6f"
+    url "https://releases.hashicorp.com/waypoint/0.10.4/waypoint_0.10.4_linux_amd64.zip"
+    sha256 "9124871b37c1b9e46489fa6cf22e2ca64c7dbe9eb39f0eb0c30a1c1e49d46448"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/waypoint/0.10.3/waypoint_0.10.3_linux_arm.zip"
-    sha256 "a4ec27f28769147d001a1a835a3fba43c6709a2aaf1e54f771cc6c6536f469a7"
+    url "https://releases.hashicorp.com/waypoint/0.10.4/waypoint_0.10.4_linux_arm.zip"
+    sha256 "cca355515f78b4d84468dc3b3d787ccb02cfff2e936101c585cce94055191307"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/waypoint/0.10.3/waypoint_0.10.3_linux_arm64.zip"
-    sha256 "f3995e1a452b22f0a99e6a79732d8e3d8573f038e913838d29315f728bb0b2ba"
+    url "https://releases.hashicorp.com/waypoint/0.10.4/waypoint_0.10.4_linux_arm64.zip"
+    sha256 "09088af249e42f970d7fe6c951af8ff76e13cc4288710a8661848d36a3bad7cc"
   end
 
   conflicts_with "waypoint"
