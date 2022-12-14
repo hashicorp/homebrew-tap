c1c31981644609bd047bcfb1ebe2c181605096f1
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2022-12-14 22:58:36 +0000

    Bump sentinel 0.19.1

--- Formula/sentinel.rb
+++ Formula/sentinel.rb
@@ -1,31 +1,31 @@
 class Sentinel < Formula
   desc ""
   homepage "https://docs.hashicorp.com/sentinel"
-  version "0.19.0"
+  version "0.19.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/sentinel/0.19.0/sentinel_0.19.0_darwin_amd64.zip"
-    sha256 "cb28d3b2a3a0940088a8fb693399609418a03abb11606cf75f995b50372df8b0"
+    url "https://releases.hashicorp.com/sentinel/0.19.1/sentinel_0.19.1_darwin_amd64.zip"
+    sha256 "5cb2d5dadd216c7d418e08e6d9729c1b74a63beb121bdd154d3d2d9f01960e26"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/sentinel/0.19.0/sentinel_0.19.0_darwin_arm64.zip"
-    sha256 "7a22f0a016522df9e0c84743521a899be05d76bff8f0d522820b5cf1935fe135"
+    url "https://releases.hashicorp.com/sentinel/0.19.1/sentinel_0.19.1_darwin_arm64.zip"
+    sha256 "f2fe24cccf1e7c963e00b0355a4b2c6d48498f8c977d50290b83cea7c9a7185e"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/sentinel/0.19.0/sentinel_0.19.0_linux_amd64.zip"
-    sha256 "4f152b82427f413b60bb04704c06a5220a36b1ea68056c0989e8fa91d54ee135"
+    url "https://releases.hashicorp.com/sentinel/0.19.1/sentinel_0.19.1_linux_amd64.zip"
+    sha256 "7f905d072b732266d39015a58326c30a217a0263ebfc5230bd39a54043ab8b04"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/sentinel/0.19.0/sentinel_0.19.0_linux_arm.zip"
-    sha256 "4ef85102e34833df7f563c067c28ccbe98d56f64dcc8b0625b1b05dc4cee4496"
+    url "https://releases.hashicorp.com/sentinel/0.19.1/sentinel_0.19.1_linux_arm.zip"
+    sha256 "b309255e52f49de39291fc0102e9bd4cf4c8cc403b7aed2177c826751da42953"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/sentinel/0.19.0/sentinel_0.19.0_linux_arm64.zip"
-    sha256 "59ad61a90a0bb88796b1bea8e4ce68babd9bb39959a445ecf91b63967a4f1bc4"
+    url "https://releases.hashicorp.com/sentinel/0.19.1/sentinel_0.19.1_linux_arm64.zip"
+    sha256 "1a6e08795332323e1b41abff639c07882d71f8601aa53203ffdc4aaae5e8d01e"
   end
 
   conflicts_with "sentinel"
