8c25d13b2f6afc4caa1967181669ac9de48fbbcd
hc-espd-packager <hc-espd-packager@users.noreply.github.com>
2023-02-21 16:25:14 +0000

    Bump vault-enterprise 1.12.3+ent.hsm

--- Formula/vault-enterprise.rb
+++ Formula/vault-enterprise.rb
@@ -1,31 +1,31 @@
 class VaultEnterprise < Formula
   desc "Vault Enterprise"
   homepage "https://www.vaultproject.io"
-  version "1.11.3+ent"
+  version "1.12.3+ent.hsm"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault/1.11.3+ent/vault_1.11.3+ent_darwin_amd64.zip"
-    sha256 "2c577de00b72c847592aee9e5ed25d716dc3427383a0cb97550e8f2fb786ca8b"
+    url "https://releases.hashicorp.com/vault/1.12.3+ent.hsm/vault_1.12.3+ent.hsm_darwin_amd64.zip"
+    sha256 ""
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/vault/1.11.3+ent/vault_1.11.3+ent_darwin_arm64.zip"
-    sha256 "82b0782b95a2c847e84ce9628d2e3b6d61e9dfb3f66fc5e3dec3838dd1e57d2e"
+    url "https://releases.hashicorp.com/vault/1.12.3+ent.hsm/vault_1.12.3+ent.hsm_darwin_arm64.zip"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault/1.11.3+ent/vault_1.11.3+ent_linux_amd64.zip"
-    sha256 "1fba33d24170081696a8684bc55291ece3eae48943c20aa4de2c999515691ccf"
+    url "https://releases.hashicorp.com/vault/1.12.3+ent.hsm/vault_1.12.3+ent.hsm_linux_amd64.zip"
+    sha256 "02bea9c1aa4e68afb6375e6987526bf0de9a093f8e54da1a56740d25b1e6e162"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault/1.11.3+ent/vault_1.11.3+ent_linux_arm.zip"
-    sha256 "f45d2164d4941d069e916e35d339748a7cc4ec84f63688fe5203bf7c1e33747f"
+    url "https://releases.hashicorp.com/vault/1.12.3+ent.hsm/vault_1.12.3+ent.hsm_linux_arm.zip"
+    sha256 ""
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault/1.11.3+ent/vault_1.11.3+ent_linux_arm64.zip"
-    sha256 "e6dade7c8af7b09cc164d2f3195ee464dcb85d63a22d3bc13c92d3cc1e1de632"
+    url "https://releases.hashicorp.com/vault/1.12.3+ent.hsm/vault_1.12.3+ent.hsm_linux_arm64.zip"
+    sha256 ""
   end
 
   conflicts_with "vault-enterprise"
