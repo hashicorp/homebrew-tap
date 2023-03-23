55f202c718f4aed8c28aa7b99720f6c9b57cadfa
Scott Macfarlane <36851+smacfarlane@users.noreply.github.com>
2023-03-23 12:47:17 -0700

    Add Copywrite header to formula/cask generator template (#211)
    * Add Copywrite header to formula/cask generator template
    
    * Extract templates to files and use go:embed
    
    Signed-off-by: Scott Macfarlane <smacfarlane@hashicorp.com>
    
    ---------
    
    Signed-off-by: Scott Macfarlane <smacfarlane@hashicorp.com>

--- util/formula_templater/formula.go
+++ util/formula_templater/formula.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"io"
 	"strings"
 	"text/template"
 )
+
+//go:embed templates/formula.tmpl
+var formulaTemplate string
+
+//go:embed templates/cask.tmpl
+var caskTemplate string
 
 func printFormula(product, version, configLocation string, out io.Writer) error {
 	shasums, err := loadShasums(product, version)
@@ -71,141 +78,3 @@ func printFormula(product, version, configLocation string, out io.Writer) error 
 
 	return t.Execute(out, productConfig)
 }
-
-const formulaTemplate = `class {{ .Name }} < Formula
-  desc "{{ .Desc }}"
-  homepage "{{ .Homepage }}"
-  version "{{ .Version }}"
-
-  {{- if and .Architectures.DarwinAmd64 .Architectures.DarwinArm64 }}
-
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/{{ .Product }}/{{ .Version }}/{{ .Product }}_{{ .Version }}_darwin_amd64.zip"
-    sha256 "{{ .Architectures.DarwinAmd64SHA }}"
-  end
-
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/{{ .Product }}/{{ .Version }}/{{ .Product }}_{{ .Version }}_darwin_arm64.zip"
-    sha256 "{{ .Architectures.DarwinArm64SHA }}"
-  end
-  {{- else }}
-
-  if OS.mac?
-    url "https://releases.hashicorp.com/{{ .Product }}/{{ .Version }}/{{ .Product }}_{{ .Version }}_darwin_amd64.zip"
-    sha256 "{{ .Architectures.DarwinAmd64SHA }}"
-  end
-
-  if OS.mac? && Hardware::CPU.arm?
-    def caveats
-      <<~EOS
-        The darwin_arm64 architecture is not supported for this product
-        at this time, however we do plan to support this in the future. The
-        darwin_amd64 binary has been installed and may work in
-        compatibility mode, but it is not fully supported.
-      EOS
-    end
-  end
-  {{- end }}
-  {{- if .Architectures.LinuxAmd64 }}
-
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/{{ .Product }}/{{ .Version }}/{{ .Product }}_{{ .Version }}_linux_amd64.zip"
-    sha256 "{{ .Architectures.LinuxAmd64SHA }}"
-  end
-  {{- end }}
-  {{- if .Architectures.LinuxArm }}
-
-  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/{{ .Product }}/{{ .Version }}/{{ .Product }}_{{ .Version }}_linux_arm.zip"
-    sha256 "{{ .Architectures.LinuxArmSHA }}"
-  end
-  {{- end }}
-  {{- if .Architectures.LinuxArm64 }}
-
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/{{ .Product }}/{{ .Version }}/{{ .Product }}_{{ .Version }}_linux_arm64.zip"
-    sha256 "{{ .Architectures.LinuxArm64SHA }}"
-  end
-
-  {{- end }}
-
-  {{- with .Depends }}
-  {{ range $index, $element := . }}
-  depends_on "{{ . }}"
-  {{- end }}
-  {{- end}}
-
-  {{- with .Recommends }}
-  {{ range $index, $element := . }}
-  depends_on "{{ . }}" => :recommended
-  {{- end }}
-  {{- end}}
-
-  {{- if .Variant }}
-
-  conflicts_with "{{ .Product }}-{{ .Variant }}"
-  {{- else }}
-
-  conflicts_with "{{ .Product }}"
-  {{- end }}
-
-  def install
-    bin.install "{{ .Product }}"
-  end
-  {{- if .ServiceArgs }}
-
-  service do
-    run [bin/"{{ .Product }}"{{ range .ServiceArgs }}, "{{ . }}"{{ end }}]
-    keep_alive successful_exit: false
-    working_dir var
-    log_path var/"log/{{ .Product }}.log"
-    error_log_path var/"log/{{ .Product }}.log"
-  end
-  {{- end }}
-
-  test do
-    system "#{bin}/{{ .Product }} --version"
-  end
-end
-`
-
-const caskTemplate = `cask "hashicorp-{{ .Product }}" do
-  version "{{ .Version }}"
-  sha256 "{{ .Architectures.DarwinAmd64SHA }}"
-
-  url "https://releases.hashicorp.com/{{ .Product }}/#{version}/{{ .Product }}_#{version}_darwin_amd64.dmg",
-      verified: "hashicorp.com/{{ .Product }}/"
-  name "{{ .Name }}"
-  desc "{{ .Desc }}"
-  homepage "{{ .Homepage }}"
-
-  {{- if eq .Product "vagrant" }}
-
-  livecheck do
-    url "https://github.com/hashicorp/{{ .Product }}"
-    strategy :git
-  end
-  {{- end }}
-
-  {{- if .CaskApp }}
-
-  app "{{ .CaskApp }}"
-  {{- else }}
-
-  pkg "{{ .CaskPkg }}"
-  {{- end }}
-
-  {{- if eq .Product "vagrant" }}
-
-  uninstall script: {
-    executable: "uninstall.tool",
-    input: ["Yes"],
-    sudo:  true,
-  },
-  pkgutil: "com.{{ .Product}}.{{ .Product }}"
-
-  zap trash: "~/.{{ .Product }}.d"
-  {{- end}}
-
-end
-`
