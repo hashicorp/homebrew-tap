55423838574a5169b6624717eaff02c63b9aebf1
hc-espd-packager <team-rel-eng@hashicorp.com>
2023-04-26 19:33:53 +0000

    Bump vault to 1.13.2

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -4,31 +4,31 @@
 class Vault < Formula
   desc "Vault"
   homepage "https://www.vaultproject.io"
-  version "1.13.1"
+  version "1.13.2"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault/1.13.1/vault_1.13.1_darwin_amd64.zip"
-    sha256 "1e36bf545cb0e0bbe74071e78a342460e38ed8e9572b22045c3c83d60f9e2c66"
+    url "https://releases.hashicorp.com/vault/1.13.2/vault_1.13.2_darwin_amd64.zip"
+    sha256 "5b9f1da25ba26b90d98bb0f04a1ff930d09389d7b769d295dd2666dbb616fcd3"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/vault/1.13.1/vault_1.13.1_darwin_arm64.zip"
-    sha256 "9825b87faf467f2ea391124b54a0f7504ffaea9e29978bab52313b0fadadb51d"
+    url "https://releases.hashicorp.com/vault/1.13.2/vault_1.13.2_darwin_arm64.zip"
+    sha256 "c8b8a395f3f6cfa07023619c22b3d1809827772bf61b692039e89eb5447eb406"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault/1.13.1/vault_1.13.1_linux_amd64.zip"
-    sha256 "cf1015d0b30806515120d4a86672ea77da1fb0559e3839ba88d8e02e94e796a6"
+    url "https://releases.hashicorp.com/vault/1.13.2/vault_1.13.2_linux_amd64.zip"
+    sha256 "f7930279de8381de7c532164b4a4408895d9606c0d24e2e9d2f9acb5dfe99b3c"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault/1.13.1/vault_1.13.1_linux_arm.zip"
-    sha256 "93d72f8588c65dbf4bd7b163a0eeaed3a24837c7c507339401619bec30acde72"
+    url "https://releases.hashicorp.com/vault/1.13.2/vault_1.13.2_linux_arm.zip"
+    sha256 "e6120caa7c3fcce6be6c43f9ca24cbbb75960e9128070f8a6ddf23b6b8747088"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault/1.13.1/vault_1.13.1_linux_arm64.zip"
-    sha256 "8a247a78dd636b3cdc01d7612f81c4cf770260af53e64e906f726a671e6f2a1f"
+    url "https://releases.hashicorp.com/vault/1.13.2/vault_1.13.2_linux_arm64.zip"
+    sha256 "5f72b0cbfd857a4f70ae06978260d56b50b36b478a3b68a158dc49d854de290d"
   end
 
   conflicts_with "vault"
