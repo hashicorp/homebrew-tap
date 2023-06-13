8ab7c546654bd4effe5d86a2b27bb0e67cec4e45
Leland Ursu <leland.ursu@gmail.com>
2023-06-13 10:08:42 -0400

    update vlt version 0.2.1 (#243)
    Co-authored-by: lursu <leland.ursu@hashicorp.com>

--- Formula/vlt.rb
+++ Formula/vlt.rb
@@ -4,31 +4,31 @@
 class Vlt < Formula
   desc "Vlt CLI"
   homepage "https://github.com/hashicorp/vlt"
-  version "0.2.0"
+  version "0.2.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vlt/0.2.0/vlt_0.2.0_darwin_amd64.zip"
-    sha256 "094d1749bab5b25f6f26668bdf7389e0103bb114f61a819ad746c91e31228d36"
+    url "https://releases.hashicorp.com/vlt/0.2.1/vlt_0.2.1_darwin_amd64.zip"
+    sha256 "66daa04f83c16d932ab06b8082aa34cb7617087d8bde1d5a96cfcca14ffeb347"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/vlt/0.2.0/vlt_0.2.0_darwin_arm64.zip"
-    sha256 "86c4bac5529abd738f64b825e1b8061eff0b04baa1700b210b262f9fe4fc7f0e"
+    url "https://releases.hashicorp.com/vlt/0.2.1/vlt_0.2.1_darwin_arm64.zip"
+    sha256 "07bf12e5aec00939f4206d8024060330e509a51aeff45730024d4ef1c2e0ff9d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vlt/0.2.0/vlt_0.2.0_linux_amd64.zip"
-    sha256 "f280be1d04f372250917ef71d78c68c2397f87fb2b9cfe34971da02f101aacc8"
+    url "https://releases.hashicorp.com/vlt/0.2.1/vlt_0.2.1_linux_amd64.zip"
+    sha256 "844ebe6a765972b5546a405cb46b5f623abb9a3c05e4ce3b19dea5235b872ad5"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vlt/0.2.0/vlt_0.2.0_linux_arm.zip"
-    sha256 "835d5d7d84ae7197cb7280596728f8e93bf0882f30d40d1e0676b99829aee4c1"
+    url "https://releases.hashicorp.com/vlt/0.2.1/vlt_0.2.1_linux_arm.zip"
+    sha256 "6f62b816fd6f2d5b4bd5230beeb956c7550e1eefe9857709b8e0c7b2e1c2bf3a"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vlt/0.2.0/vlt_0.2.0_linux_arm64.zip"
-    sha256 "b9f66c836cb788ad26c92807334b958e01d26074d7785476cb391c611f5710e8"
+    url "https://releases.hashicorp.com/vlt/0.2.1/vlt_0.2.1_linux_arm64.zip"
+    sha256 "4a6486f5dc8d8e78044ed22dae0ef8681946178dbee24b08a7973e4ba889ab1d"
   end
 
   conflicts_with "vlt"
