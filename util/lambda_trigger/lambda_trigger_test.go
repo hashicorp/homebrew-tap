0fbcce8ac6f37b64e6e96a8437d1de7ecce39ba6
Chris Roberts <croberts@hashicorp.com>
2023-06-14 14:41:35 -0700

    Add more test coverage for lambda trigger

--- util/lambda_trigger/lambda_trigger_test.go
+++ util/lambda_trigger/lambda_trigger_test.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"os"
 	"testing"
 	"time"
 
@@ -270,4 +276,306 @@ func TestGetLatestVersionIgnoresVariants(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.Equal(t, "1.13.3+ent", *gotLatest)
+}
+
+func Test_isProductSupported(t *testing.T) {
+	require := require.New(t)
+	t.Run("when product is supported", func(t *testing.T) {
+		require.True(isProductSupported("vault"))
+	})
+
+	t.Run("when product is not supported", func(t *testing.T) {
+		require.False(isProductSupported("unknown-product"))
+	})
+}
+
+func Test_isFormula(t *testing.T) {
+	require := require.New(t)
+	t.Run("when product is formula only", func(t *testing.T) {
+		require.True(isFormula("vault"))
+	})
+
+	t.Run("when product is formula and cask", func(t *testing.T) {
+		require.True(isFormula("vagrant"))
+	})
+
+	t.Run("when product is cask only", func(t *testing.T) {
+		require.False(isFormula("boundary-desktop"))
+	})
+}
+
+func Test_isCask(t *testing.T) {
+	require := require.New(t)
+	t.Run("when product is formula only", func(t *testing.T) {
+		require.False(isCask("vault"))
+	})
+
+	t.Run("when product is formula and cask", func(t *testing.T) {
+		require.True(isCask("vagrant"))
+	})
+
+	t.Run("when product is cask only", func(t *testing.T) {
+		require.True(isCask("boundary-desktop"))
+	})
+}
+
+func TestHandleLambdaEvent(t *testing.T) {
+	require := require.New(t)
+	successfulTest := "test was succesful"
+
+	t.Run("when product is formula only", func(t *testing.T) {
+		t.Run("when versions are different", func(t *testing.T) {
+			reader, writer, cleanup := enableStdoutCapture(t)
+			defer cleanup()
+			defer gock.Off()
+			// Mock all the expected http requests
+			generateFormulaMock("nomad").Reply(200).
+				BodyString(generateFormulaReply("nomad", "1.3.5"))
+			generateReleasesLatestMock("nomad").Reply(200).
+				BodyString(generateReleasesReply("nomad", "1.3.6"))
+			generateDispatchMock().
+				MatchType("json").
+				BodyString(`.*"cask":"false".*`). // ensure cask job is not triggered
+				Reply(200).
+				BodyString(successfulTest)
+
+			require.NoError(HandleLambdaEvent(generateSnsEvent("nomad", "1.3.6")))
+			writer.Close()
+			result, err := ioutil.ReadAll(reader)
+			require.NoError(err)
+			require.Contains(string(result), successfulTest)
+		})
+
+		t.Run("when versions are the same", func(t *testing.T) {
+			defer gock.Off()
+			generateFormulaMock("nomad").Reply(200).
+				BodyString(generateFormulaReply("nomad", "1.3.5"))
+			generateReleasesLatestMock("nomad").Reply(200).
+				BodyString(generateReleasesReply("nomad", "1.3.5"))
+
+			err := HandleLambdaEvent(generateSnsEvent("nomad", "1.3.5"))
+			require.Error(err)
+			require.Contains(err.Error(), "latest version")
+		})
+	})
+
+	t.Run("when product is cask only", func(t *testing.T) {
+		t.Run("when versions are different", func(t *testing.T) {
+			reader, writer, cleanup := enableStdoutCapture(t)
+			defer cleanup()
+			defer gock.Off()
+			generateCaskMock("boundary-desktop").Reply(200).
+				BodyString(generateCaskReply("boundary-desktop", "1.1.1"))
+			generateReleasesLatestMock("boundary-desktop").Reply(200).
+				BodyString(generateReleasesReply("boundary-desktop", "1.1.2"))
+			generateDispatchMock().
+				MatchType("json").
+				BodyString(`.*"cask":"true".*`). // ensure cask job is triggered
+				Reply(200).
+				BodyString(successfulTest)
+
+			require.NoError(HandleLambdaEvent(generateSnsEvent("boundary-desktop", "1.1.2")))
+			writer.Close()
+			result, err := ioutil.ReadAll(reader)
+			require.NoError(err)
+			require.Contains(string(result), successfulTest)
+		})
+
+		t.Run("when versions are the same", func(t *testing.T) {
+			defer gock.Off()
+			generateCaskMock("boundary-desktop").Reply(200).
+				BodyString(generateCaskReply("boundary-desktop", "1.1.1"))
+			generateReleasesLatestMock("boundary-desktop").Reply(200).
+				BodyString(generateReleasesReply("boundary-desktop", "1.1.1"))
+
+			err := HandleLambdaEvent(generateSnsEvent("boundary-desktop", "1.1.2"))
+			require.Error(err)
+			require.Contains(err.Error(), "latest version")
+		})
+	})
+
+	t.Run("when product is formula and cask", func(t *testing.T) {
+		formulaSuccess := "formula test success"
+		caskSuccess := "cask test success"
+		t.Run("when versions are different", func(t *testing.T) {
+			t.Run("it should trigger workflow twice", func(t *testing.T) {
+				reader, writer, cleanup := enableStdoutCapture(t)
+				defer cleanup()
+				defer gock.Off()
+				generateCaskMock("vagrant").Reply(200).
+					BodyString(generateCaskReply("vagrant", "1.0.1"))
+				generateReleasesLatestMock("vagrant").Reply(200).
+					BodyString(generateReleasesReply("vagrant", "1.0.2"))
+				generateDispatchMock().Reply(200).
+					BodyString(caskSuccess)
+				generateDispatchMock().Reply(200).
+					BodyString(formulaSuccess)
+
+				require.NoError(HandleLambdaEvent(generateSnsEvent("vagrant", "1.0.2")))
+				writer.Close()
+				result, err := ioutil.ReadAll(reader)
+				require.NoError(err)
+				require.Contains(string(result), caskSuccess)
+				require.Contains(string(result), formulaSuccess)
+			})
+
+			t.Run("it should trigger cask workflow", func(t *testing.T) {
+				reader, writer, cleanup := enableStdoutCapture(t)
+				defer cleanup()
+				defer gock.Off()
+				generateCaskMock("vagrant").Reply(200).
+					BodyString(generateCaskReply("vagrant", "1.0.1"))
+				generateReleasesLatestMock("vagrant").Reply(200).
+					BodyString(generateReleasesReply("vagrant", "1.0.2"))
+				generateDispatchMock().
+					MatchType("json").
+					BodyString(`.*"cask":"true".*`).
+					Reply(200).
+					BodyString(caskSuccess)
+				generateDispatchMock().
+					Reply(200).
+					BodyString(formulaSuccess)
+
+				require.NoError(HandleLambdaEvent(generateSnsEvent("vagrant", "1.0.2")))
+				writer.Close()
+				result, err := ioutil.ReadAll(reader)
+				require.NoError(err)
+				require.Contains(string(result), caskSuccess)
+			})
+
+			t.Run("it should trigger formula workflow", func(t *testing.T) {
+				reader, writer, cleanup := enableStdoutCapture(t)
+				defer cleanup()
+				defer gock.Off()
+				generateCaskMock("vagrant").Reply(200).
+					BodyString(generateCaskReply("vagrant", "1.0.1"))
+				generateReleasesLatestMock("vagrant").Reply(200).
+					BodyString(generateReleasesReply("vagrant", "1.0.2"))
+				generateDispatchMock().
+					Reply(200).
+					BodyString(caskSuccess)
+				generateDispatchMock().
+					MatchType("json").
+					BodyString(`.*"cask":"false".*`).
+					Reply(200).
+					BodyString(formulaSuccess)
+
+				require.NoError(HandleLambdaEvent(generateSnsEvent("vagrant", "1.0.2")))
+				writer.Close()
+				result, err := ioutil.ReadAll(reader)
+				require.NoError(err)
+				require.Contains(string(result), formulaSuccess)
+			})
+		})
+
+		t.Run("when versions are the same", func(t *testing.T) {
+			defer gock.Off()
+			generateCaskMock("vagrant").Reply(200).
+				BodyString(generateCaskReply("vagrant", "1.0.1"))
+			generateReleasesLatestMock("vagrant").Reply(200).
+				BodyString(generateReleasesReply("vagrant", "1.0.1"))
+			err := HandleLambdaEvent(generateSnsEvent("vagrant", "1.0.2"))
+			require.Error(err)
+			require.Contains(err.Error(), "latest version")
+		})
+	})
+}
+
+// Captures content written to stdout
+func enableStdoutCapture(t *testing.T) (reader io.ReadCloser, writer io.Closer, cleanup func()) {
+	t.Helper()
+	reader, w, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("Could not setup stdout capture: %s", err)
+	}
+	writer = w
+	stdout := os.Stdout
+	os.Stdout = w
+	cleanup = func() {
+		os.Stdout = stdout
+		reader.Close()
+		writer.Close()
+	}
+	return
+}
+
+// Generate simplified formula content for version checking
+func generateFormulaReply(product, version string) string {
+	return fmt.Sprintf(`desc "%s"\nversion "%s"\n`, product, version)
+}
+
+// Generate simplified cask content for version checking
+// NOTE: hashicorp- prefix is automatically added to product
+func generateCaskReply(product, version string) string {
+	// Casks always have "hashicorp-" prefix so add
+	product = "hashicorp-" + product
+	return fmt.Sprintf(`cask "%s" do\nversion "%s"\nend`, product, version)
+}
+
+// Generates the response body for latest from releases api
+// NOTE: It only includes the name and version values
+func generateReleasesReply(product, version string) string {
+	info := map[string]string{
+		"name":    product,
+		"version": version,
+	}
+	payload, err := json.Marshal(info)
+	if err != nil {
+		panic(err)
+	}
+	return string(payload)
+}
+
+// Generates an SNS event message using provided product and version
+func generateSnsEvent(product, version string) events.SNSEvent {
+	info := map[string]string{
+		"product": product,
+		"version": version,
+	}
+	payload, err := json.Marshal(info)
+	if err != nil {
+		panic(err)
+	}
+
+	return events.SNSEvent{
+		Records: []events.SNSEventRecord{
+			{
+				SNS: events.SNSEntity{
+					Message: string(payload),
+				},
+			},
+		},
+	}
+}
+
+// Generates the base mock used when requesting latest version from the releases api
+func generateReleasesLatestMock(product string) *gock.Request {
+	return gock.New("https://api.releases.hashicorp.com").
+		Get(fmt.Sprintf("/v1/releases/%s/latest", product))
+}
+
+// Generates the base mock used when requesting formula for version check
+func generateFormulaMock(product string) *gock.Request {
+	return gock.New("https://raw.githubusercontent.com").
+		Get(fmt.Sprintf("/hashicorp/homebrew-tap/master/Formula/%s.rb", product))
+}
+
+// Generates the base mock used when requesting cask for version check
+// NOTE: hashicorp- prefix is automatically added to product
+func generateCaskMock(product string) *gock.Request {
+	// Casks always have "hashicorp-" prefix so add
+	product = "hashicorp-" + product
+	return gock.New("https://raw.githubusercontent.com").
+		Get(fmt.Sprintf("/hashicorp/homebrew-tap/master/Casks/%s.rb", product))
+}
+
+// Generates the base mock used when sending a repository dispatch notification
+func generateDispatchMock() *gock.Request {
+	dispatchUrl, err := url.Parse(workflowEndpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	return gock.New(fmt.Sprintf("%s://%s", dispatchUrl.Scheme, dispatchUrl.Host)).
+		Post(dispatchUrl.Path)
 }
