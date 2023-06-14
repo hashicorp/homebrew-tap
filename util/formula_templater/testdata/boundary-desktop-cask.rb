e560589d9d8b10b9a3f6a44dd6ce140301066f2d
Chris Roberts <croberts@hashicorp.com>
2023-06-14 14:50:56 -0700

    Add tests for cask printing and formula without mac support

---
+++ util/formula_templater/testdata/boundary-desktop-cask.rb
@@ -0,0 +1,16 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+cask "hashicorp-boundary-desktop" do
+  version "1.6.0"
+  sha256 "b6b5b15dfb469b7fdab9216788f5931e96561104de1ff7f9e0d6fda54701be09"
+
+  url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_amd64.dmg",
+      verified: "hashicorp.com/boundary-desktop/"
+  name "Boundary Desktop"
+  desc ""
+  homepage "https://www.boundaryproject.io/"
+
+  app "Boundary.app"
+
+end
