eec1c7749d2684ec05e6d7e76c240e6531557b18
hc-espd-packager <team-rel-eng@hashicorp.com>
2023-08-08 19:14:51 +0000

    Bump consul-enterprise to 1.16.1+ent

--- Formula/consul-enterprise.rb
+++ Formula/consul-enterprise.rb
@@ -4,31 +4,31 @@
 class ConsulEnterprise < Formula
   desc "Consul Enterprise"
   homepage "https://www.consul.io"
-  version "1.16.0+ent"
+  version "1.16.1+ent"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/consul/1.16.0+ent/consul_1.16.0+ent_darwin_amd64.zip"
-    sha256 "0be682d4441b5d3a5779b29f3f1d8a43283da8fdd58113e9b796c5256018049e"
+    url "https://releases.hashicorp.com/consul/1.16.1+ent/consul_1.16.1+ent_darwin_amd64.zip"
+    sha256 "87adc902f312bc95534cb2890b3d915162b8b7d7f849cc49245e6403b57c6b8a"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/consul/1.16.0+ent/consul_1.16.0+ent_darwin_arm64.zip"
-    sha256 "37d9af36c0aca9ec419bf4a8027f94394ef6a3dff1df6e4d705ae5dfb5e1f0df"
+    url "https://releases.hashicorp.com/consul/1.16.1+ent/consul_1.16.1+ent_darwin_arm64.zip"
+    sha256 "9629641080f02d7b87d50ac3d5a933a57b0607f8fac01f4c8e5078a1ea749615"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/consul/1.16.0+ent/consul_1.16.0+ent_linux_amd64.zip"
-    sha256 "f71ede1daac8428da54f28dd98573a29c2806489314355244c90fa984b4fdfb0"
+    url "https://releases.hashicorp.com/consul/1.16.1+ent/consul_1.16.1+ent_linux_amd64.zip"
+    sha256 "3e68670d0d723363adea8e8edc89848d784e90e382b00294a0994b701cbc47f0"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul/1.16.0+ent/consul_1.16.0+ent_linux_arm.zip"
-    sha256 "7e1ca3305bca1ef08b603ec665048a3d3ce94857cb962e15855152f04dbba3f9"
+    url "https://releases.hashicorp.com/consul/1.16.1+ent/consul_1.16.1+ent_linux_arm.zip"
+    sha256 "74f66f09027e63c66f425a78124120e7e6d8aa6fe4844567a6ee0da8417b9c13"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul/1.16.0+ent/consul_1.16.0+ent_linux_arm64.zip"
-    sha256 "2161cc8bd29820d2a0aedc8db6a8ebdd82f37998fe9e70b24708511f5a8e05c8"
+    url "https://releases.hashicorp.com/consul/1.16.1+ent/consul_1.16.1+ent_linux_arm64.zip"
+    sha256 "e85da5ac98a1c5dcaddbebc126c807e5c1171443f45090bf5617755fe6472eed"
   end
 
   conflicts_with "consul-enterprise"
