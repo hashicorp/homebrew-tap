7abbd8463f8afcdd681869a1cd6c6e4ea99b71b3
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-01-15 20:40:22 +0000

    Bump nomad to 1.7.3

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -4,31 +4,31 @@
 class Nomad < Formula
   desc "Nomad"
   homepage "https://www.nomadproject.io/"
-  version "1.7.2"
+  version "1.7.3"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad/1.7.2/nomad_1.7.2_darwin_amd64.zip"
-    sha256 "16c5a5cbccb505972ef2699ae60b8dd411a3ad4a3cebda3add1b15fcef9a8866"
+    url "https://releases.hashicorp.com/nomad/1.7.3/nomad_1.7.3_darwin_amd64.zip"
+    sha256 "6759d54aebb804220f55b5d1cd0deeaf97ce4420d47744ebd8204d4c3b8962df"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/nomad/1.7.2/nomad_1.7.2_darwin_arm64.zip"
-    sha256 "85e976783ca1acf3965dbdfcb5ea0edd48c594739ecece84461f621e930d820f"
+    url "https://releases.hashicorp.com/nomad/1.7.3/nomad_1.7.3_darwin_arm64.zip"
+    sha256 "b423450bdb334a5a43e8f836e9c7be70914f874a95a7d6ee8b827079ddea576d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad/1.7.2/nomad_1.7.2_linux_amd64.zip"
-    sha256 "5264b4f4b9a0bf8f086544f15e6a6377c856e5556bf44337c958f5356d251331"
+    url "https://releases.hashicorp.com/nomad/1.7.3/nomad_1.7.3_linux_amd64.zip"
+    sha256 "ee0d93fe1b2d5aead6bc5c0508c8ac99822e11157c0e1172f5da0d7d139c26c8"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/nomad/1.7.2/nomad_1.7.2_linux_arm.zip"
-    sha256 "c81951736cdd3906e84d065ff2f742f424bf0216e8541a4930055a69698f091d"
+    url "https://releases.hashicorp.com/nomad/1.7.3/nomad_1.7.3_linux_arm.zip"
+    sha256 "3be2463363dc91f49c34010938c1875adf58114e296a82f4b206d697b2481d58"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/nomad/1.7.2/nomad_1.7.2_linux_arm64.zip"
-    sha256 "5cdcf9103c8252c83bf1c8540288c3fc2e64b69828beb65395646c76beaff2a0"
+    url "https://releases.hashicorp.com/nomad/1.7.3/nomad_1.7.3_linux_arm64.zip"
+    sha256 "5d986363290109cbcdce889b1e7b6ec01b86a3759ec56930c78ae0bebfb65f15"
   end
 
   conflicts_with "nomad"
