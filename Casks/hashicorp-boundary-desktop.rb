4bc5fcec9b65b151d9ea8aefca59d8aac92acd61
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-01-31 19:30:22 +0000

    Bump boundary-desktop to 2.0.0

--- Casks/hashicorp-boundary-desktop.rb
+++ Casks/hashicorp-boundary-desktop.rb
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: MPL-2.0
 
 cask "hashicorp-boundary-desktop" do
-  version "1.7.1"
-  sha256 "1308d08a5ddb86da04af68f325200ebd33f724cd4c7ea3d609fcf1f0dce36a3e"
+  version "2.0.0"
+  sha256 "49656e489440dcf773658cfa51d1abcfbadff5aef32127f2844f4f16ac170ae6"
 
   url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_amd64.dmg",
       verified: "hashicorp.com/boundary-desktop/"
