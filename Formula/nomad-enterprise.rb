6fa0b4e5a7ec606ce43964053f358ad826b8e8c0
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-02-13 16:29:21 +0000

    Bump nomad-enterprise to 1.7.5+ent

--- Formula/nomad-enterprise.rb
+++ Formula/nomad-enterprise.rb
@@ -4,31 +4,31 @@
 class NomadEnterprise < Formula
   desc "Nomad Enterprise"
   homepage "https://www.nomadproject.io/"
-  version "1.7.4+ent"
+  version "1.7.5+ent"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad/1.7.4+ent/nomad_1.7.4+ent_darwin_amd64.zip"
-    sha256 "22f32f0887cdafd757ee56d1d8de0d133c8ed963f3b3eec84d6840cac289750f"
+    url "https://releases.hashicorp.com/nomad/1.7.5+ent/nomad_1.7.5+ent_darwin_amd64.zip"
+    sha256 "749564ae16342f25837ba9b91a32ff9032fda388a4b2efb1e255e33b6a6fa7f2"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/nomad/1.7.4+ent/nomad_1.7.4+ent_darwin_arm64.zip"
-    sha256 "4edad470b50cd3e8e7a0d47d03691fb9b456117da63b45b623dd0653a4809467"
+    url "https://releases.hashicorp.com/nomad/1.7.5+ent/nomad_1.7.5+ent_darwin_arm64.zip"
+    sha256 "82cfc4f1d385779741e347435bfdd9d39e724b07bf64ed8cbfeafc15c710d414"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad/1.7.4+ent/nomad_1.7.4+ent_linux_amd64.zip"
-    sha256 "1140eee2c97e205a5bbff42b6d4e58de1891483336387f8e008d9010c99ac2a3"
+    url "https://releases.hashicorp.com/nomad/1.7.5+ent/nomad_1.7.5+ent_linux_amd64.zip"
+    sha256 "bd8d63e7905ccc874ef63f6979cf8c492e4084d049291f155f95b2e71281aedd"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/nomad/1.7.4+ent/nomad_1.7.4+ent_linux_arm.zip"
-    sha256 "18b8f69b1797de23995a1e00282547bec351d014d9de716b958bc6cf1e002744"
+    url "https://releases.hashicorp.com/nomad/1.7.5+ent/nomad_1.7.5+ent_linux_arm.zip"
+    sha256 "884d4a67799ad47aca28efde1549fdefc83b30a830e624026aad262130d0db68"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/nomad/1.7.4+ent/nomad_1.7.4+ent_linux_arm64.zip"
-    sha256 "8b07836027aa01d91502e7eba7b36a8b04fbf541730468fb9afe42a805be1a1f"
+    url "https://releases.hashicorp.com/nomad/1.7.5+ent/nomad_1.7.5+ent_linux_arm64.zip"
+    sha256 "81580d0495b4b145b6ef608f2f1b1ef4c637ded23f7ffb62041a7d788cab51b9"
   end
 
   conflicts_with "nomad-enterprise"
