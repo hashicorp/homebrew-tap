1b377ce7373908f4100b177ff54229b1d41da78e
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-02-21 20:20:11 +0000

    Bump terraform to 1.7.4

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -4,31 +4,31 @@
 class Terraform < Formula
   desc "Terraform"
   homepage "https://www.terraform.io/"
-  version "1.7.3"
+  version "1.7.4"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/terraform/1.7.3/terraform_1.7.3_darwin_amd64.zip"
-    sha256 "4787f5a422439d3b277a889b159981e88049f48bcf9e41e70481620567a7fd9c"
+    url "https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_darwin_amd64.zip"
+    sha256 "fcf35c8b1d3f46fb84f688312ef5f829081d2d56c10444b9f6e290e21e68871c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/terraform/1.7.3/terraform_1.7.3_darwin_arm64.zip"
-    sha256 "85cddfd303c45989f0948a70ae03bb30f66c6e6106383697fe85ccd739137ca6"
+    url "https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_darwin_arm64.zip"
+    sha256 "3f25268a5d7677cc89d39a505362979acfb02f19ddb965d7ec0b33a6d9e64075"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/terraform/1.7.3/terraform_1.7.3_linux_amd64.zip"
-    sha256 "617042989ce46b5dd07772237b49b57b8f8e97b1604c9dbbd85ead87effb51fe"
+    url "https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_linux_amd64.zip"
+    sha256 "285539a6fd62fb79f05edc15cc207ca90f282901c32261085ea0642a0d638dfd"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/terraform/1.7.3/terraform_1.7.3_linux_arm.zip"
-    sha256 "1a738382f1b9d1fd5238e1408db939768ce5ee9d5d2c6e0db1f29a68690afea4"
+    url "https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_linux_arm.zip"
+    sha256 "b8f281a58d9642f3edb437b25796cf189db35dddc81feb6a3d5472029f609283"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/terraform/1.7.3/terraform_1.7.3_linux_arm64.zip"
-    sha256 "e9a8a2f676b51a5334d00a0c3695b24ca75e30f4f449eb191e304fedfa099565"
+    url "https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_linux_arm64.zip"
+    sha256 "36680616b54c5ce8c8222c7bf81c187727b3c5c1a3a6e3af5b5372caa28697b7"
   end
 
   conflicts_with "terraform"
