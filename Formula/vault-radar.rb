c9f8e633aa23b86249a2c92126989163e7817bf6
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-05-13 16:27:20 +0000

    Bump vault-radar to 0.7.0

--- Formula/vault-radar.rb
+++ Formula/vault-radar.rb
@@ -4,26 +4,26 @@
 class VaultRadar < Formula
   desc "Vault Radar"
   homepage "https://developer.hashicorp.com/hcp/docs/vault-radar/cli"
-  version "0.6.0"
+  version "0.7.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault-radar/0.6.0/vault-radar_0.6.0_darwin_amd64.zip"
-    sha256 "d7b4a0d662ab574a122a2ab0057fe17203a389c4856fead4e5cbcea3195e4c9d"
+    url "https://releases.hashicorp.com/vault-radar/0.7.0/vault-radar_0.7.0_darwin_amd64.zip"
+    sha256 "4c21b099f21945760ed2acc82bdb24f45b01c5da64d017e5ed1c6444c4fd055c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/vault-radar/0.6.0/vault-radar_0.6.0_darwin_arm64.zip"
-    sha256 "f5df9ab542fff33735ee8857f40e68e07a018df2005dfbe8aea52ae441ef39c7"
+    url "https://releases.hashicorp.com/vault-radar/0.7.0/vault-radar_0.7.0_darwin_arm64.zip"
+    sha256 "35be665458d0a30e9c333665da32271aad4e4d88eaaa6af6b4de9721516776ca"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault-radar/0.6.0/vault-radar_0.6.0_linux_amd64.zip"
-    sha256 "fff741226d5f140955874c9d1104feb1ee7e56820814a1c4d2e0b4164b0c0184"
+    url "https://releases.hashicorp.com/vault-radar/0.7.0/vault-radar_0.7.0_linux_amd64.zip"
+    sha256 "869a8a4c3e6da907160e6fa07dfc81327d9de94f84d8d9deb958eb5805bebd3d"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault-radar/0.6.0/vault-radar_0.6.0_linux_arm64.zip"
-    sha256 "8849f23e3ee9b0e7ff98f459bf7c666bcb971f9d91df81774bd897c9a512c3a0"
+    url "https://releases.hashicorp.com/vault-radar/0.7.0/vault-radar_0.7.0_linux_arm64.zip"
+    sha256 "08e845f0d846a90c35ffcc93a63df7acc3af81e8e4ae37b9f7df670452bbfb8b"
   end
 
   conflicts_with "vault-radar"
