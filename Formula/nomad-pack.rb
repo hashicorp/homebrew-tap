318db11d4944ab9b1756f887205f54e6d2ad3f7a
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-06-11 18:02:28 +0000

    Bump nomad-pack to 0.1.2

--- Formula/nomad-pack.rb
+++ Formula/nomad-pack.rb
@@ -4,31 +4,26 @@
 class NomadPack < Formula
   desc "Nomad Pack"
   homepage "https://github.com/hashicorp/nomad-pack"
-  version "0.1.1"
+  version "0.1.2"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad-pack/0.1.1/nomad-pack_0.1.1_darwin_amd64.zip"
-    sha256 "c72aac2149aca00ebeee948bbfa6103a7e5191049cc0ab2b884956853d6204c0"
+    url "https://releases.hashicorp.com/nomad-pack/0.1.2/nomad-pack_0.1.2_darwin_amd64.zip"
+    sha256 "98ce44bf0a35a3d0148c766bdfd3fa330f9da00bcb2269b065ba55ed7d20eed8"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/nomad-pack/0.1.1/nomad-pack_0.1.1_darwin_arm64.zip"
-    sha256 "3596512e54a074fc6c3a69c79ed43c1e794edfc4e534766ebc1711286780558a"
+    url "https://releases.hashicorp.com/nomad-pack/0.1.2/nomad-pack_0.1.2_darwin_arm64.zip"
+    sha256 "12fb5cce35cc532513bb392a35c9c3af7a7381f232077c49b12510ba802dc82b"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad-pack/0.1.1/nomad-pack_0.1.1_linux_amd64.zip"
-    sha256 "0028be2d07845918edff55177fd1db73315f7079dbb9a1dbe70c9cd362221246"
-  end
-
-  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/nomad-pack/0.1.1/nomad-pack_0.1.1_linux_arm.zip"
-    sha256 "095549eaf3021545468de2dc1c9bbcc30160b4e8e2979fe7b0b13ec9a16664c3"
+    url "https://releases.hashicorp.com/nomad-pack/0.1.2/nomad-pack_0.1.2_linux_amd64.zip"
+    sha256 "7b89d9652e8622a99270fbbbc7fb457383d9f624faceaa41d2292bacd37a51ae"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/nomad-pack/0.1.1/nomad-pack_0.1.1_linux_arm64.zip"
-    sha256 "6784cdcc00f075e9d2d03a7d945b41a450a058a499418a2f3a8b573188b516f4"
+    url "https://releases.hashicorp.com/nomad-pack/0.1.2/nomad-pack_0.1.2_linux_arm64.zip"
+    sha256 "bb1410a1efc80299792fd929e49d915e4db3a4a560a5de1c45e4628ff1f1e5fa"
   end
 
   conflicts_with "nomad-pack"
