e48153b0d140657dce1ccad76d0f99bf3362d223
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-06-25 17:46:36 +0000

    Bump hcp to 0.4.0

--- Formula/hcp.rb
+++ Formula/hcp.rb
@@ -4,31 +4,31 @@
 class Hcp < Formula
   desc "HCP CLI"
   homepage "https://github.com/hashicorp/hcp"
-  version "0.3.0"
+  version "0.4.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/hcp/0.3.0/hcp_0.3.0_darwin_amd64.zip"
-    sha256 "d408f4bbd15c461efbdd422dc90fc5a5af775e4d15f53b47d9fa1eb3b642c219"
+    url "https://releases.hashicorp.com/hcp/0.4.0/hcp_0.4.0_darwin_amd64.zip"
+    sha256 "8cc36144872e513ec1a564c2086307af511c55f59e6aa735ab2e3e6db4760d88"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/hcp/0.3.0/hcp_0.3.0_darwin_arm64.zip"
-    sha256 "73033275d5b8f83305c0951fd870297d109d0b683f237f46e943136fb98a25a8"
+    url "https://releases.hashicorp.com/hcp/0.4.0/hcp_0.4.0_darwin_arm64.zip"
+    sha256 "256ae425f8a8efde5636725ac2a3e59082b79af955419a8fb5795ac8b66b571c"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/hcp/0.3.0/hcp_0.3.0_linux_amd64.zip"
-    sha256 "7491ec2d4860b2b14b28464d7e89a4f3687b345154c393344114f7a0df2b8fa3"
+    url "https://releases.hashicorp.com/hcp/0.4.0/hcp_0.4.0_linux_amd64.zip"
+    sha256 "f60ed93b57d73d3d5fa26df59b09dc3b16f9b4ee2eadfef6b5ab216b62913352"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/hcp/0.3.0/hcp_0.3.0_linux_arm.zip"
-    sha256 "d8135c451a478285d8a2eafca224a342056357266baab8a8cbe11a76702a0d55"
+    url "https://releases.hashicorp.com/hcp/0.4.0/hcp_0.4.0_linux_arm.zip"
+    sha256 "76d1ca62821751c08e511bd8a48277cc74d0584bccf51be2fee59ac1e6fef3ec"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/hcp/0.3.0/hcp_0.3.0_linux_arm64.zip"
-    sha256 "491e34db09e544218d32c99cbf27231f5cae0d33065b31298bd8605afda1a91e"
+    url "https://releases.hashicorp.com/hcp/0.4.0/hcp_0.4.0_linux_arm64.zip"
+    sha256 "2c1c08802be382f5965a6c8cd875db2cdf0ffadd3f433cfffb45d5ee8664d801"
   end
 
   conflicts_with "hcp"
