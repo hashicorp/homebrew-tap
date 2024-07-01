5fa0b14a58c5f590148dcdb1af229408831224f2
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-07-01 15:45:54 +0000

    Bump packer to 1.11.1

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -4,31 +4,31 @@
 class Packer < Formula
   desc "Packer"
   homepage "https://www.packer.io/"
-  version "1.11.0"
+  version "1.11.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/packer/1.11.0/packer_1.11.0_darwin_amd64.zip"
-    sha256 "7f2a9bcaf339864da0458f676d8b6581a2c274929327fb15a9a7d24e8ea8dcf5"
+    url "https://releases.hashicorp.com/packer/1.11.1/packer_1.11.1_darwin_amd64.zip"
+    sha256 "08daab53fa0edeeb95d44d93dfe78e2ac7b667e1c35c6a19c7edc665967e7424"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/packer/1.11.0/packer_1.11.0_darwin_arm64.zip"
-    sha256 "d4933bbd36fa63ff904ba94fb02630a2489491eedf01af79c4cfb674bf989476"
+    url "https://releases.hashicorp.com/packer/1.11.1/packer_1.11.1_darwin_arm64.zip"
+    sha256 "86262421358282ce7e64a80505902486dd91ece808d188443747c47ab80e686d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/packer/1.11.0/packer_1.11.0_linux_amd64.zip"
-    sha256 "dcac06a4c671bbb71e916da5abe947ebf4d6aa35c197e21c7c7b1d68cb8b7cad"
+    url "https://releases.hashicorp.com/packer/1.11.1/packer_1.11.1_linux_amd64.zip"
+    sha256 "07a9d92fe98d7bb4be09392c06ef6c4f9ffbc905fe6c29fff0622432367f01cc"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/packer/1.11.0/packer_1.11.0_linux_arm.zip"
-    sha256 "644ff27386993ef09b29169c383b70c0c92aef6bce53bdd9f87b945c4a6a31bb"
+    url "https://releases.hashicorp.com/packer/1.11.1/packer_1.11.1_linux_arm.zip"
+    sha256 "ae820f1f97aae5b674cf76ac69797ecf42c85a1e0671f0499302a803a88a3e63"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/packer/1.11.0/packer_1.11.0_linux_arm64.zip"
-    sha256 "0edd69e9cf34687d118610ed5de88fc18a2d3e2d9d3600ff3e76eaac08afd5cb"
+    url "https://releases.hashicorp.com/packer/1.11.1/packer_1.11.1_linux_arm64.zip"
+    sha256 "2666f40bde53f4684ba5a3d52a38c02d6ba45fd114c38ea6a0925bedd2e16ad0"
   end
 
   conflicts_with "packer"
