5002d7adbe1ee78be1757099e73d6a07eaa84e16
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-07-30 20:19:19 +0000

    Bump hcdiag to 0.5.4

--- Formula/hcdiag.rb
+++ Formula/hcdiag.rb
@@ -4,31 +4,31 @@
 class Hcdiag < Formula
   desc "Hcdiag"
   homepage "https://github.com/hashicorp/hcdiag"
-  version "0.5.3"
+  version "0.5.4"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/hcdiag/0.5.3/hcdiag_0.5.3_darwin_amd64.zip"
-    sha256 "2e0ba85d2425881c0a0481689bab09438d0fa05c14d5982975a2a443129414d5"
+    url "https://releases.hashicorp.com/hcdiag/0.5.4/hcdiag_0.5.4_darwin_amd64.zip"
+    sha256 "db36ddd6ad619840c8295ad375fab4749c52a92397a5cac29c37601f8ce922e6"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/hcdiag/0.5.3/hcdiag_0.5.3_darwin_arm64.zip"
-    sha256 "c22634ac3236fa5beb99ef83bc802369d78c281f7403bdc351ad4ad0aadaa445"
+    url "https://releases.hashicorp.com/hcdiag/0.5.4/hcdiag_0.5.4_darwin_arm64.zip"
+    sha256 "1d0faf988509eb545797c078891945523e52f82a9a99db9c2fc533c174e0fd23"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/hcdiag/0.5.3/hcdiag_0.5.3_linux_amd64.zip"
-    sha256 "e8b7e1cf7cd3216f0179a0d5fb44cb0d6b5425b7fd023849d2078f0f27d7cb63"
+    url "https://releases.hashicorp.com/hcdiag/0.5.4/hcdiag_0.5.4_linux_amd64.zip"
+    sha256 "162d3a9d8ca586633c85fedaf40ac6baf9b858d4e4c1ce0a4de3b77e03c85854"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/hcdiag/0.5.3/hcdiag_0.5.3_linux_arm.zip"
-    sha256 "233b94d5d0426eac582b91838eec70a9ccdd5333be1d214a177acffe5d9fb210"
+    url "https://releases.hashicorp.com/hcdiag/0.5.4/hcdiag_0.5.4_linux_arm.zip"
+    sha256 "4d615d1f57dbda436925b6a6257428f237d4700016a570d725fcd8426d5f90cc"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/hcdiag/0.5.3/hcdiag_0.5.3_linux_arm64.zip"
-    sha256 "0f6c53142fbc9d6d4748f16571df8e6903a4dc1373c20c9256f8c3424e0cdc54"
+    url "https://releases.hashicorp.com/hcdiag/0.5.4/hcdiag_0.5.4_linux_arm64.zip"
+    sha256 "6ff93833a098c0664d74fb3691dcabb13ef158bb66e04b3081362f7ad5213758"
   end
 
   conflicts_with "hcdiag"
