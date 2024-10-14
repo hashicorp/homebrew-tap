0265da480ba939e658370d87e148bae0450c0aac
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-10-14 14:04:27 +0000

    Bump boundary to 0.17.2

--- Formula/boundary.rb
+++ Formula/boundary.rb
@@ -4,31 +4,31 @@
 class Boundary < Formula
   desc "Boundary"
   homepage "https://www.boundaryproject.io/"
-  version "0.17.1"
+  version "0.17.2"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/boundary/0.17.1/boundary_0.17.1_darwin_amd64.zip"
-    sha256 "47afd877823fc88b90f5f56711105efe9c1099fbbbaf10021a4e45ddbf622b49"
+    url "https://releases.hashicorp.com/boundary/0.17.2/boundary_0.17.2_darwin_amd64.zip"
+    sha256 "5d36a4491ed294680eb0212fe3f866d77f5ba55b0dfe2ed7a522b712bc5aa181"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/boundary/0.17.1/boundary_0.17.1_darwin_arm64.zip"
-    sha256 "b179c2e263d964437e0e25f0e74f04c402663d064fff369d1e7677e5e7055044"
+    url "https://releases.hashicorp.com/boundary/0.17.2/boundary_0.17.2_darwin_arm64.zip"
+    sha256 "e5d7eb9c3ced8dee166b81a613aecbc60a8ba0c91ceb4abbec5d7034290abca0"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/boundary/0.17.1/boundary_0.17.1_linux_amd64.zip"
-    sha256 "8125803b2642b811cdb8ea8efc4bbf015fda50c72fe21e34f1cbe061c55e7b98"
+    url "https://releases.hashicorp.com/boundary/0.17.2/boundary_0.17.2_linux_amd64.zip"
+    sha256 "55caca1a952017b0362a857ecc4b10ba4c8eac6b394eeb632b4befc31c48b6b1"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/boundary/0.17.1/boundary_0.17.1_linux_arm.zip"
-    sha256 "35cb4d7ba9ef4309e45b0af3b536b9e71f14b369488736b9e3deca9d65308ac2"
+    url "https://releases.hashicorp.com/boundary/0.17.2/boundary_0.17.2_linux_arm.zip"
+    sha256 "ae222db9d191c7c06a62ab1be0f7de5d86b471a296865cb2192cf9b291c9ca90"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/boundary/0.17.1/boundary_0.17.1_linux_arm64.zip"
-    sha256 "7b383989ec7b3646e94f4b2ad0899c50fbdcd364ba9bb504868ba019102c9f3f"
+    url "https://releases.hashicorp.com/boundary/0.17.2/boundary_0.17.2_linux_arm64.zip"
+    sha256 "e11b2e816db785b91ecae6247901be83e7d088c9cbbac2b0202e5dab84b41178"
   end
 
   conflicts_with "boundary"
