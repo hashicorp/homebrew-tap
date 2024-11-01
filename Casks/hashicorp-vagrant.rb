6484f2f0fef0239f027f9b1cb451e19aa39e7ae5
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-11-01 22:59:41 +0000

    Bump vagrant to 2.4.2

--- Casks/hashicorp-vagrant.rb
+++ Casks/hashicorp-vagrant.rb
@@ -2,10 +2,10 @@
 # SPDX-License-Identifier: MPL-2.0
 
 cask "hashicorp-vagrant" do
-  version "2.4.1"
+  version "2.4.2"
   arch arm: "arm64", intel: "amd64"
-  sha256 arm: "881953f7d4cea45aa8b2f2c6c8f5714c2ad4586edd17c6faeb163c6ebed2918e",
-         intel: "881953f7d4cea45aa8b2f2c6c8f5714c2ad4586edd17c6faeb163c6ebed2918e"
+  sha256 arm: "dec702353ed087d1e1a18a22c3af75d644eb8004ddbcb2cf337c0bfc471842d3",
+         intel: "dec702353ed087d1e1a18a22c3af75d644eb8004ddbcb2cf337c0bfc471842d3"
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_darwin_#{arch}.dmg",
       verified: "hashicorp.com/vagrant/"
   name "Vagrant"
