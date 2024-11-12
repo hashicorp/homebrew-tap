f7c14f932bd2a44fc137657e371de0beddad144b
hc-espd-packager <team-rel-eng@hashicorp.com>
2024-11-12 21:47:23 +0000

    Bump vagrant to 2.4.3

--- Formula/vagrant.rb
+++ Formula/vagrant.rb
@@ -4,11 +4,11 @@
 class Vagrant < Formula
   desc "Development environment"
   homepage "https://www.vagrantup.com/"
-  version "2.4.2"
+  version "2.4.3"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vagrant/2.4.2/vagrant_2.4.2_linux_amd64.zip"
-    sha256 "4289a3378b8b3397bbb64bfb2eb8fbd79ff271665a2765a35bda45e47d791d00"
+    url "https://releases.hashicorp.com/vagrant/2.4.3/vagrant_2.4.3_linux_amd64.zip"
+    sha256 "ae4fb15990cbbeaed5ab6d22524d30c59879a28ff7ed8972284fc055261219ef"
   end
 
   conflicts_with "vagrant"
