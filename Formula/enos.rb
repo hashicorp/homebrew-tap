3d6018b7e13022980884ea0fb47f1f07935b142e
hc-github-team-secure-quality <82992790+hc-github-team-secure-quality@users.noreply.github.com>
2024-11-22 15:50:07 -0700

    Update Homebrew formula for Enos version v0.0.33 (#296)
    Co-authored-by: Vault Quality Team <team-vault-quality@hashicorp.com>

--- Formula/enos.rb
+++ Formula/enos.rb
@@ -1,28 +1,28 @@
 class Enos < Formula
   desc "A tool for powering Software Quality as Code by writing Terraform-based quality requirement scenarios using a composable, modular, and declarative language."
   homepage "https://github.com/hashicorp/enos"
-  version "0.0.32"
+  version "0.0.33"
 
   depends_on "hashicorp/tap/terraform"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/hashicorp/enos/releases/download/v0.0.32/enos_0.0.32_darwin_arm64.zip"
-    sha256 "cb6bf99cc11972111ef43743cce9c5d3f6ca797ddadfb77673d8822ec48630a8"
+    url "https://github.com/hashicorp/enos/releases/download/v0.0.33/enos_0.0.33_darwin_arm64.zip"
+    sha256 "3562a6751c33ed1b2e6af5131c7b47b98018336ce6f0df51651263b2c1ac262c"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/hashicorp/enos/releases/download/v0.0.32/enos_0.0.32_darwin_amd64.zip"
-    sha256 "9fa946f3ca1a70cce9cda1684566b9b91645e5cb9ef8dbceaa74eb358ad0263a"
+    url "https://github.com/hashicorp/enos/releases/download/v0.0.33/enos_0.0.33_darwin_amd64.zip"
+    sha256 "30384eb2a0e9d49018493fbb986637f68b45092176d933ce28c1643018eb4c59"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/hashicorp/enos/releases/download/v0.0.32/enos_0.0.32_linux_arm64.zip"
-    sha256 "1a8353f89e3fbf16b7fd745188eaf919437c337e0a7faecffa2bcb251e59dbf0"
+    url "https://github.com/hashicorp/enos/releases/download/v0.0.33/enos_0.0.33_linux_arm64.zip"
+    sha256 "21c955ec28ccb71aacad8556c314fff6d6f1b7bd16b5e3d43841c6b1cfa50e47"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/hashicorp/enos/releases/download/v0.0.32/enos_0.0.32_linux_amd64.zip"
-    sha256 "93b6051b659c7f7f24925bd78b4b0c1ff114db5b15b6396f6e5e0db7f71a9938"
+    url "https://github.com/hashicorp/enos/releases/download/v0.0.33/enos_0.0.33_linux_amd64.zip"
+    sha256 "74c1a2f3bbeafc449362c500690637310d2194f1f19954c5ea0c94236a332ff2"
   end
 
   def install
