6332b08387aa36a8235efa4b73b64f5f63cd779f
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2024-12-04 08:43:26 +0000

    Bump tfstacks to 0.6.0

--- Formula/tfstacks.rb
+++ Formula/tfstacks.rb
@@ -4,31 +4,31 @@
 class Tfstacks < Formula
   desc "Terraform Stacks CLI"
   homepage "https://www.terraform.io/"
-  version "0.5.2"
+  version "0.6.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/tfstacks/0.5.2/tfstacks_0.5.2_darwin_amd64.zip"
-    sha256 "aa5011dbf3720173f1a8108ddbcf5f079e39e89baa9fec2758d9f8926ffcf3e5"
+    url "https://releases.hashicorp.com/tfstacks/0.6.0/tfstacks_0.6.0_darwin_amd64.zip"
+    sha256 "12cf50f51d14c33c72cce196200be594b6b713183d6f396168625f04a7298d9e"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/tfstacks/0.5.2/tfstacks_0.5.2_darwin_arm64.zip"
-    sha256 "05a2c881e0443fc57c7377fa7e04112586aeaac86160e87ce5d455612a4b4346"
+    url "https://releases.hashicorp.com/tfstacks/0.6.0/tfstacks_0.6.0_darwin_arm64.zip"
+    sha256 "a39a6aec9dcc75277eb49bdb55a86bfcdaba6100395cf673062e9589a8dfd8e4"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/tfstacks/0.5.2/tfstacks_0.5.2_linux_amd64.zip"
-    sha256 "2e65201116a0bb56e8378370e1d2c59e9054ef5bb14495f3b656788fe930acdf"
+    url "https://releases.hashicorp.com/tfstacks/0.6.0/tfstacks_0.6.0_linux_amd64.zip"
+    sha256 "442479de2183ca95e22493e26d81e6d8780952200c619add97181f11400c8c1b"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/tfstacks/0.5.2/tfstacks_0.5.2_linux_arm.zip"
-    sha256 "0df0ce9fe68228a5eed23eb1f32f778af0e6e4b1163624f5b677abd421de6c90"
+    url "https://releases.hashicorp.com/tfstacks/0.6.0/tfstacks_0.6.0_linux_arm.zip"
+    sha256 "6ec7a85cf6f45c0574e3807d81657a87aae12b11983021b35549b3e5bcbeea40"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/tfstacks/0.5.2/tfstacks_0.5.2_linux_arm64.zip"
-    sha256 "80b276954c5af688c65fb7a8949b213410a990d7291a38e0b38b169e795f3c9f"
+    url "https://releases.hashicorp.com/tfstacks/0.6.0/tfstacks_0.6.0_linux_arm64.zip"
+    sha256 "78304307c4f97ae8da6c3cb57e44b2f3d55506f55479494bbcbead6cde1fe8cd"
   end
 
   conflicts_with "tfstacks"
