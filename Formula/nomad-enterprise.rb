1f06c518b8cd5cb72763d7d6a0e859070d884eb2
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2024-12-18 20:45:57 +0000

    Bump nomad-enterprise to 1.9.4+ent

--- Formula/nomad-enterprise.rb
+++ Formula/nomad-enterprise.rb
@@ -4,26 +4,26 @@
 class NomadEnterprise < Formula
   desc "Nomad Enterprise"
   homepage "https://www.nomadproject.io/"
-  version "1.9.3+ent"
+  version "1.9.4+ent"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad/1.9.3+ent/nomad_1.9.3+ent_darwin_amd64.zip"
-    sha256 "39e0629ba88e6428d642d6cca630da18c075943197cafe954b0603f6beb342f4"
+    url "https://releases.hashicorp.com/nomad/1.9.4+ent/nomad_1.9.4+ent_darwin_amd64.zip"
+    sha256 "3597c3e8dc83115fb1d6ab25761b179bdda348ca29347f6206949fcef07e7c0b"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/nomad/1.9.3+ent/nomad_1.9.3+ent_darwin_arm64.zip"
-    sha256 "ad66da9be43515db1e025c8445e3fb5efa46a53d8b00723e63d5f7a4c28551e1"
+    url "https://releases.hashicorp.com/nomad/1.9.4+ent/nomad_1.9.4+ent_darwin_arm64.zip"
+    sha256 "4b6db762c1f81674cb2eaab401b180e0e147f122ecc977d66fa1cc60be8bb888"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad/1.9.3+ent/nomad_1.9.3+ent_linux_amd64.zip"
-    sha256 "0f60544a4c46e5bba344b2b53c48a11017c4a6df2ef2f6accd4662bd24fd887f"
+    url "https://releases.hashicorp.com/nomad/1.9.4+ent/nomad_1.9.4+ent_linux_amd64.zip"
+    sha256 "11d4d6f419f372b872d1877e9a794503eebdfc9930c67b4b38f86c9116937c66"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/nomad/1.9.3+ent/nomad_1.9.3+ent_linux_arm64.zip"
-    sha256 "af62f21a34092fdd8d2392a818592153bacf35a0d191b1de8b8a9d77706bfcde"
+    url "https://releases.hashicorp.com/nomad/1.9.4+ent/nomad_1.9.4+ent_linux_arm64.zip"
+    sha256 "b0ec294f8e156126fe11bd594cb89cc81e22999cb13877568a917027873d7e09"
   end
 
   conflicts_with "nomad-enterprise"
