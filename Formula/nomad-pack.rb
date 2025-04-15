91ced83737e6cf6e6e4d5eb06c8a053a9db681b4
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2025-04-15 20:34:34 +0000

    Bump nomad-pack to 0.3.0

--- Formula/nomad-pack.rb
+++ Formula/nomad-pack.rb
@@ -4,26 +4,26 @@
 class NomadPack < Formula
   desc "Nomad Pack"
   homepage "https://github.com/hashicorp/nomad-pack"
-  version "0.2.0"
+  version "0.3.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad-pack/0.2.0/nomad-pack_0.2.0_darwin_amd64.zip"
-    sha256 "e0a1a7db24783b5b9faaa13b49050eb9b643be02357c5d7dd3c0de5ad4d9e6d9"
+    url "https://releases.hashicorp.com/nomad-pack/0.3.0/nomad-pack_0.3.0_darwin_amd64.zip"
+    sha256 "16263eb87e15d677eec480aa2a79b0387329d222a100baa25daee4445b183edf"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/nomad-pack/0.2.0/nomad-pack_0.2.0_darwin_arm64.zip"
-    sha256 "68d33c69c616377267c77a2ccb63e3a4365406764f495ae21d19a890274e49a0"
+    url "https://releases.hashicorp.com/nomad-pack/0.3.0/nomad-pack_0.3.0_darwin_arm64.zip"
+    sha256 "a928fabcc61fcbc9ff226e063af23a21b6c4ac57a9608ac9656408f9c37c7061"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad-pack/0.2.0/nomad-pack_0.2.0_linux_amd64.zip"
-    sha256 "32533e635b78101056f411366e26aca7778ea80f8556002f825784e1d75a4437"
+    url "https://releases.hashicorp.com/nomad-pack/0.3.0/nomad-pack_0.3.0_linux_amd64.zip"
+    sha256 "0a4d83ce937e93c83248b9fbe7e80750767d26e57af07f59f24583426d531ea6"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/nomad-pack/0.2.0/nomad-pack_0.2.0_linux_arm64.zip"
-    sha256 "63c918f90d790d70b87f7c550366ad03238aab8a3eb0edb70ecad009659e453c"
+    url "https://releases.hashicorp.com/nomad-pack/0.3.0/nomad-pack_0.3.0_linux_arm64.zip"
+    sha256 "018ed23c485abb7d8354d471051a6aab3cb604195f1d071e556f6417eccc5894"
   end
 
   conflicts_with "nomad-pack"
