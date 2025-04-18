fa21bab1256e2c5b664061f8357195342b33f095
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2025-04-18 18:38:06 +0000

    Bump vault-enterprise to 1.19.2+ent

--- Formula/vault-enterprise.rb
+++ Formula/vault-enterprise.rb
@@ -4,31 +4,31 @@
 class VaultEnterprise < Formula
   desc "Vault Enterprise"
   homepage "https://www.vaultproject.io"
-  version "1.19.1+ent"
+  version "1.19.2+ent"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault/1.19.1+ent/vault_1.19.1+ent_darwin_amd64.zip"
-    sha256 "cd080087a8d254e60eb82aeafe49a0116c5aa8a456acc218194d633708dfaf0d"
+    url "https://releases.hashicorp.com/vault/1.19.2+ent/vault_1.19.2+ent_darwin_amd64.zip"
+    sha256 "f857169d152ab445c79358b5fdcdd4d194fa7854316ab430aa7ca3cdfa0d5d60"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/vault/1.19.1+ent/vault_1.19.1+ent_darwin_arm64.zip"
-    sha256 "c12e05faf1a3decd1710f784a67782a1fb0b7ce31b3cbc899aa97a5f377d5687"
+    url "https://releases.hashicorp.com/vault/1.19.2+ent/vault_1.19.2+ent_darwin_arm64.zip"
+    sha256 "6af3f4e04e3a282c03deeedb7d0e3c11c4a7ae793c9ff2e0505f61257dadf406"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault/1.19.1+ent/vault_1.19.1+ent_linux_amd64.zip"
-    sha256 "0d773c41b3b03885f39c766b6583a5aa138838865cc773fe9a3b9e6494e6030f"
+    url "https://releases.hashicorp.com/vault/1.19.2+ent/vault_1.19.2+ent_linux_amd64.zip"
+    sha256 "5ff9fd20728f55128ce0c55602e72bc8310b0e5ed06f1142b6909dfb7f549cd1"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault/1.19.1+ent/vault_1.19.1+ent_linux_arm.zip"
-    sha256 "f21e7b82be4b56c28828f439da7935e19bca00fb610d31b1e5842d1daa7e80b3"
+    url "https://releases.hashicorp.com/vault/1.19.2+ent/vault_1.19.2+ent_linux_arm.zip"
+    sha256 "18c465feae422dc5ae76b018917af61dcfee57a436d9be2bae8200a103f0aa52"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault/1.19.1+ent/vault_1.19.1+ent_linux_arm64.zip"
-    sha256 "ec072286fd796a6577b6c2aa816a525888837207e6098acc07bd233adf0cf12a"
+    url "https://releases.hashicorp.com/vault/1.19.2+ent/vault_1.19.2+ent_linux_arm64.zip"
+    sha256 "251be28653c4c06850bc93498d94af39d8b723d324a5a2c7970b43c1fed759fc"
   end
 
   conflicts_with "vault-enterprise"
