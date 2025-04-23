5f4e994e852c57997c91b68d388cdc4d9f347a41
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2025-04-23 20:29:43 +0000

    Bump vagrant to 2.4.5

--- Casks/hashicorp-vagrant.rb
+++ Casks/hashicorp-vagrant.rb
@@ -2,10 +2,10 @@
 # SPDX-License-Identifier: MPL-2.0
 
 cask "hashicorp-vagrant" do
-  version "2.4.4"
+  version "2.4.5"
   arch arm: "arm64", intel: "amd64"
-  sha256 arm: "955b1011273721928dbf3316194bf402e1f2a817a7ba98e6e1863d4413e54a44",
-         intel: "955b1011273721928dbf3316194bf402e1f2a817a7ba98e6e1863d4413e54a44"
+  sha256 arm: "abe3a21b54ade42c16d50562f34a821c071bed568104c799ef9ca2e013fb9f9d",
+         intel: "abe3a21b54ade42c16d50562f34a821c071bed568104c799ef9ca2e013fb9f9d"
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_darwin_#{arch}.dmg",
       verified: "hashicorp.com/vagrant/"
   name "Vagrant"
