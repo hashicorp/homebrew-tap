4ad397ac9485212ab9fd9e65e7b6ef1b9bab81b1
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2025-05-13 10:59:54 +0000

    Bump nomad-enterprise to 1.10.1+ent

--- Formula/nomad-enterprise.rb
+++ Formula/nomad-enterprise.rb
@@ -4,26 +4,26 @@
 class NomadEnterprise < Formula
   desc "Nomad Enterprise"
   homepage "https://www.nomadproject.io/"
-  version "1.10.0+ent"
+  version "1.10.1+ent"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad/1.10.0+ent/nomad_1.10.0+ent_darwin_amd64.zip"
-    sha256 "3b81b6f0b4207cf4a4d060a8167b291272e7a6f607ea7a448698b691658405b0"
+    url "https://releases.hashicorp.com/nomad/1.10.1+ent/nomad_1.10.1+ent_darwin_amd64.zip"
+    sha256 "860612a4f85e9989726082d92c7e106c50ec27a5cde468518d31dc4d80403940"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/nomad/1.10.0+ent/nomad_1.10.0+ent_darwin_arm64.zip"
-    sha256 "75ca96e9272b1ed4357e85bff50242e6f06056aac0320bb488a07743d5099983"
+    url "https://releases.hashicorp.com/nomad/1.10.1+ent/nomad_1.10.1+ent_darwin_arm64.zip"
+    sha256 "76374c6400cee0ea566526e39deb6d1d17362fa02eb4fae74956eb3bba458a59"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/nomad/1.10.0+ent/nomad_1.10.0+ent_linux_amd64.zip"
-    sha256 "d599bbe69c1373170d1798c332f74021447d5167428a70225fd10cdc2c5fd50b"
+    url "https://releases.hashicorp.com/nomad/1.10.1+ent/nomad_1.10.1+ent_linux_amd64.zip"
+    sha256 "e24021bea9b463037332dbf91418307d1019bf8e7f93405bd28602cb2eb6cdc0"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/nomad/1.10.0+ent/nomad_1.10.0+ent_linux_arm64.zip"
-    sha256 "1f886ce27c797baccfe9f88c3753f7e0c6ba468c23a05884cf17b894ab9222a4"
+    url "https://releases.hashicorp.com/nomad/1.10.1+ent/nomad_1.10.1+ent_linux_arm64.zip"
+    sha256 "62f419ea7f05a1e545e848536b2e187e98954f8ec3b188c006e3e6357e2aaf8f"
   end
 
   conflicts_with "nomad-enterprise"
