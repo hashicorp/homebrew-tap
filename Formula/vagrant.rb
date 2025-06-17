7bd23190ab6c87da0e455d038155fca0ee5557c3
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2025-06-17 17:23:36 +0000

    Bump vagrant to 2.4.7

--- Formula/vagrant.rb
+++ Formula/vagrant.rb
@@ -4,11 +4,11 @@
 class Vagrant < Formula
   desc "Development environment"
   homepage "https://www.vagrantup.com/"
-  version "2.4.6"
+  version "2.4.7"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vagrant/2.4.6/vagrant_2.4.6_linux_amd64.zip"
-    sha256 "34ebb5b731b06d3c8b462885d33b1d468d186513c4d2c3101121df9121fa34a9"
+    url "https://releases.hashicorp.com/vagrant/2.4.7/vagrant_2.4.7_linux_amd64.zip"
+    sha256 "3ecafba03ff8185b516d45d75abde6a4e1d28379009baaf3614b83449bd9295f"
   end
 
   conflicts_with "vagrant"
