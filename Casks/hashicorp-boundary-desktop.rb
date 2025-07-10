ebd5464adc5cef6ccdec9158ec3f66e0f87fb5dd
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2025-07-10 17:56:48 +0000

    Bump boundary-desktop to 2.3.3

--- Casks/hashicorp-boundary-desktop.rb
+++ Casks/hashicorp-boundary-desktop.rb
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: MPL-2.0
 
 cask "hashicorp-boundary-desktop" do
-  version "2.3.2"
-  sha256 "7050e2a69373fa93df4a612f3219d73a3fcc91b3729322ec5be8e939ee254973"
+  version "2.3.3"
+  sha256 "738bfdcad34004e3f550f38c8f5dced9c7454e1dbc567a52f5a9539c21c8cc28"
 
   url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_amd64.dmg",
       verified: "hashicorp.com/boundary-desktop/"
