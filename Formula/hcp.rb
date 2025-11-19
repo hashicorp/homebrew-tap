287c7741b95d8e5a1d74f3b2b72e78cb352700f4
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2025-11-19 00:14:46 +0000

    Bump hcp to 0.11.0

--- Formula/hcp.rb
+++ Formula/hcp.rb
@@ -4,31 +4,31 @@
 class Hcp < Formula
   desc "HCP CLI"
   homepage "https://github.com/hashicorp/hcp"
-  version "0.10.0"
+  version "0.11.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/hcp/0.10.0/hcp_0.10.0_darwin_amd64.zip"
-    sha256 "1440bebd4859920fdeb7118a7da8a84e7680328e24ef5b29f85115b4bd71b162"
+    url "https://releases.hashicorp.com/hcp/0.11.0/hcp_0.11.0_darwin_amd64.zip"
+    sha256 "5dc1d68c848eebf33a8897885f51ba243b82abe2f5db6546b404729b7bfbe6a4"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/hcp/0.10.0/hcp_0.10.0_darwin_arm64.zip"
-    sha256 "e054b7499d0c9da432d9bd84782e30e11ca2538f1097ea327710b225225d4eec"
+    url "https://releases.hashicorp.com/hcp/0.11.0/hcp_0.11.0_darwin_arm64.zip"
+    sha256 "064fb59443d437d71a532ae054ba888fe993ce34164cb858c54ea416226a2417"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/hcp/0.10.0/hcp_0.10.0_linux_amd64.zip"
-    sha256 "d9f0af27a87c4bf43ab15139985c99a4c70b0e3e37b64d1a97e6978550fb79fe"
+    url "https://releases.hashicorp.com/hcp/0.11.0/hcp_0.11.0_linux_amd64.zip"
+    sha256 "09a0d2bd8a8834907e162188c4d68468824444aca163b28b8c38ba63749fb052"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/hcp/0.10.0/hcp_0.10.0_linux_arm.zip"
-    sha256 "c48fc1b10c56158c412e2267edd15f673df8e77b87a7d02814f14b8b6b88b9e1"
+    url "https://releases.hashicorp.com/hcp/0.11.0/hcp_0.11.0_linux_arm.zip"
+    sha256 "15f8eae1b4abc91f4a03380b583503304cd16e5cad468845c34c1eb67766daa3"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/hcp/0.10.0/hcp_0.10.0_linux_arm64.zip"
-    sha256 "3c9becec5ce1feb9481600bc2b39c71fbed025c3183382478edcaf8dc11425c9"
+    url "https://releases.hashicorp.com/hcp/0.11.0/hcp_0.11.0_linux_arm64.zip"
+    sha256 "2a2528756df22dbab5cfb0645ae3098c811a5f033c9a98e6171c6aa8f728bf5e"
   end
 
   conflicts_with "hcp"
