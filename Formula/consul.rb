da4f4a4aed68e31a5eaa9098d55d473761cb3b3b
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2025-11-27 00:47:44 +0000

    Bump consul to 1.22.1

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -4,31 +4,31 @@
 class Consul < Formula
   desc "Consul"
   homepage "https://www.consul.io"
-  version "1.22.0"
+  version "1.22.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/consul/1.22.0/consul_1.22.0_darwin_amd64.zip"
-    sha256 "eb949ef495d53c1a08d9564105e742ed9eb93ef4f05abafaa4c479b3895bb4b8"
+    url "https://releases.hashicorp.com/consul/1.22.1/consul_1.22.1_darwin_amd64.zip"
+    sha256 "1d8f48639618b8d293e3cfe7855df9fb53c802262848053bf45c260fd7427493"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/consul/1.22.0/consul_1.22.0_darwin_arm64.zip"
-    sha256 "6d9d58a2364d2ac23c816d6eb463ffcd10c568c547268fad310dc4299ac483e5"
+    url "https://releases.hashicorp.com/consul/1.22.1/consul_1.22.1_darwin_arm64.zip"
+    sha256 "1bbbdc43ac23321f402d661b20dd0d291047ecbda2dbfe93e4b4ce26cf2c701d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/consul/1.22.0/consul_1.22.0_linux_amd64.zip"
-    sha256 "9891495a2defabc3d637b376c66550e9879102868fbe6456a9a683067ae20ae9"
+    url "https://releases.hashicorp.com/consul/1.22.1/consul_1.22.1_linux_amd64.zip"
+    sha256 "91222c7ec141f1c2c92f6b732eeb0251220337e4c07c768cbc6ae633fef69733"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul/1.22.0/consul_1.22.0_linux_arm.zip"
-    sha256 "f57b7e2fd5e28ff451ebb626cc17bcf707124ee5096f80abdb2cb44dd9db3eb5"
+    url "https://releases.hashicorp.com/consul/1.22.1/consul_1.22.1_linux_arm.zip"
+    sha256 "b677d91e3613103ce24fd3cd1d66d12047985d17970ae8c8986fb8282a9b79bd"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/consul/1.22.0/consul_1.22.0_linux_arm64.zip"
-    sha256 "9481aec98bd198d919332e1bed75a62825a9b44cc341b1d56bbfbfb81c5a14c9"
+    url "https://releases.hashicorp.com/consul/1.22.1/consul_1.22.1_linux_arm64.zip"
+    sha256 "d421c4537b57935a949b9bc34252266672fe484a766a05ec5826bbae16c8f323"
   end
 
   conflicts_with "consul"
