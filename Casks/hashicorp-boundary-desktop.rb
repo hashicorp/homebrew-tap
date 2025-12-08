0c1ec9b377ab892f870e3769da9758f4c80e8faa
hashicc <191911133+hashicc@users.noreply.github.com>
2025-12-08 12:17:14 -0500

    Update sha hashes for current arm and intel boundary downloads (#329)
    * Update sha hashes for current arm and intel boundary downloads
    
    * Update config to include arm builds in boundary-desktop

--- Casks/hashicorp-boundary-desktop.rb
+++ Casks/hashicorp-boundary-desktop.rb
@@ -3,7 +3,8 @@
 
 cask "hashicorp-boundary-desktop" do
   version "2.4.0"
-  sha256 "f3559241ee4ee2f08f07c91b663d4c97d8091fa677143c2c9ecf083160b8833d"
+  sha256 arm: "d886fb8082733d9a87e7430403fe3eba00904e973c45cb8331062a985883b29a",
+         intel: "f3559241ee4ee2f08f07c91b663d4c97d8091fa677143c2c9ecf083160b8833d"
   arch arm: "arm64", intel: "amd64"
 
   url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_#{arch}.dmg",
