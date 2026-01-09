9f2f9db668484ba1966fcb1768bd612db37647c9
latsss <99077252+latsss@users.noreply.github.com>
2026-01-09 22:39:05 +0000

    Bump Vagrant to 2.4.9

--- Formula/vagrant.rb
+++ Formula/vagrant.rb
@@ -4,11 +4,11 @@
 class Vagrant < Formula
   desc "Development environment"
   homepage "https://www.vagrantup.com/"
-  version "2.4.8"
+  version "2.4.9"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vagrant/2.4.8/vagrant_2.4.8_linux_amd64.zip"
-    sha256 "95d7e0c1b47bb7f9cfab9fe23d5a0f877f79aa6e5ef9ffe4b960fb8fd5db44f4"
+    url "https://releases.hashicorp.com/vagrant/2.4.9/vagrant_2.4.9_linux_amd64.zip"
+    sha256 "77d4d533c82c420b6b594992a902ec43fcd9f50380dc002a599e93fc744f8cfa"
   end
 
   conflicts_with "vagrant"
