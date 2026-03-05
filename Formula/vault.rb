ef24df3a4251413609888ac3fe97ad6367ec799e
Ashley Connor <86969811+ashconnor@users.noreply.github.com>
2026-03-05 11:40:11 -0800

    Bump Vault and Vault-Enterprise to 1.21.4 (#344)

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -4,26 +4,26 @@
 class Vault < Formula
   desc "Vault"
   homepage "https://www.vaultproject.io"
-  version "1.21.1"
+  version "1.21.4"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault/1.21.1/vault_1.21.1_darwin_amd64.zip"
-    sha256 "d33bb27a0ad194e79c2bed9cad198a1f1319d8ca68bc6c4e6f68212c734cda09"
+    url "https://releases.hashicorp.com/vault/1.21.4/vault_1.21.4_darwin_amd64.zip"
+    sha256 "a667be3cf56dd0f21a23ba26b47028d1f51b3ca61e71b0e29ceafef1c2a1dc3a"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/vault/1.21.1/vault_1.21.1_darwin_arm64.zip"
-    sha256 "add728e2ca2101826de030b4da6de77cee5a61f3c9cde74f5628d63332bea0ab"
+    url "https://releases.hashicorp.com/vault/1.21.4/vault_1.21.4_darwin_arm64.zip"
+    sha256 "c79012c1c8aedd682c68b5d9c89149030611c82da57f45383aef004b39a640d2"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault/1.21.1/vault_1.21.1_linux_amd64.zip"
-    sha256 "4088617653eba4ea341b6166130239fcbe42edc7839c7f7c6209d280948769c7"
+    url "https://releases.hashicorp.com/vault/1.21.4/vault_1.21.4_linux_amd64.zip"
+    sha256 "889b681990fe221b884b7932fa9c9dd0ee9811b9349554f1aa287ab63c9f3dae"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault/1.21.1/vault_1.21.1_linux_arm64.zip"
-    sha256 "f83f541e4293289bf1cc3f1e62e41a29a9ce20aeb9a152ada2b00ca42e7e856d"
+    url "https://releases.hashicorp.com/vault/1.21.4/vault_1.21.4_linux_arm64.zip"
+    sha256 "1104ef701aad16e104e2e7b4d2a02a6ec993237559343f3097ac63a00b42e85d"
   end
 
   conflicts_with "vault"
