85a48ea68dca2bbdcb55ff841d60837d13a864f5
hc-github-team-es-release-engineering <github-team-es-release-engineering@hashicorp.com>
2026-05-04 23:13:32 +0000

    Bump vault-radar to 0.47.0

--- Formula/vault-radar.rb
+++ Formula/vault-radar.rb
@@ -4,26 +4,26 @@
 class VaultRadar < Formula
   desc "Vault Radar"
   homepage "https://developer.hashicorp.com/hcp/docs/vault-radar/cli"
-  version "0.46.0"
+  version "0.47.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault-radar/0.46.0/vault-radar_0.46.0_darwin_amd64.zip"
-    sha256 "70314ca277880066fb56ddd871f801d3b0e2459c6c0fbe8dd1ee38e0a159db6e"
+    url "https://releases.hashicorp.com/vault-radar/0.47.0/vault-radar_0.47.0_darwin_amd64.zip"
+    sha256 "c563142789ab3b9df73495ae77167294b6c934114377a8adf979e37300989dd1"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/vault-radar/0.46.0/vault-radar_0.46.0_darwin_arm64.zip"
-    sha256 "32505021fc850ebef70a5559d6ad9215341f52a35d42aaa229ba6ab7fc814dfa"
+    url "https://releases.hashicorp.com/vault-radar/0.47.0/vault-radar_0.47.0_darwin_arm64.zip"
+    sha256 "388111bc2f34ea737252d2734f2b68b4818cc46bee621fb60346b9c6d537627d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/vault-radar/0.46.0/vault-radar_0.46.0_linux_amd64.zip"
-    sha256 "932c337589610c316d906c661eaac55ab588728c330b9bbfc94150bad876a51a"
+    url "https://releases.hashicorp.com/vault-radar/0.47.0/vault-radar_0.47.0_linux_amd64.zip"
+    sha256 "c45b1221402ff39df269964a64e7291d5d10be467d2a9b86275ef8c1f1119bac"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/vault-radar/0.46.0/vault-radar_0.46.0_linux_arm64.zip"
-    sha256 "a18ba8ca5ad63920dadaebffa2e77ab75eca03543d7e866cb74c80ace012ac94"
+    url "https://releases.hashicorp.com/vault-radar/0.47.0/vault-radar_0.47.0_linux_arm64.zip"
+    sha256 "2a4583775b1d68075f0fc747a1cd63d9c73d356881bdeed681569b97403e8a0d"
   end
 
   conflicts_with "vault-radar"
