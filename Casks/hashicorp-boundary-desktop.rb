75023a6c71701927f36348a05ef25724489a1291
Niklas Wagner <Skaro@Skaronator.com>
2026-06-10 17:56:02 +0200

    fix: wrap macOS-only stanzas in on_macos for Linux tap validation

--- Casks/hashicorp-boundary-desktop.rb
+++ Casks/hashicorp-boundary-desktop.rb
@@ -3,15 +3,19 @@
 
 cask "hashicorp-boundary-desktop" do
   version "2.6.0"
-  arch arm: "arm64", intel: "amd64"
-  sha256 arm: "a292b940f04cec8e7770de1c691cb91df0169dc57e5c161740956d7fb7768235",
-         intel: "f5d236ef1c40784100485e4247de3d3ea325c6a2ebd664d7895cca2a469b7830"
-  url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_#{arch}.dmg",
-      verified: "hashicorp.com/boundary-desktop/"
+
+  on_macos do
+    arch arm: "arm64", intel: "amd64"
+    sha256 arm: "a292b940f04cec8e7770de1c691cb91df0169dc57e5c161740956d7fb7768235",
+           intel: "f5d236ef1c40784100485e4247de3d3ea325c6a2ebd664d7895cca2a469b7830"
+    url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_#{arch}.dmg",
+        verified: "hashicorp.com/boundary-desktop/"
+
+    app "Boundary.app"
+  end
+
   name "Boundary Desktop"
   desc ""
   homepage "https://www.boundaryproject.io/"
-
-  app "Boundary.app"
 
 end
--- Casks/hashicorp-vagrant.rb
+++ Casks/hashicorp-vagrant.rb
@@ -3,11 +3,26 @@
 
 cask "hashicorp-vagrant" do
   version "2.4.9"
-  arch arm: "arm64", intel: "amd64"
-  sha256 arm: "8de08bd435ef8ae0fc5fbd6acefa9c68e62fb898c5ae0fbdacd26853bea9d4d6",
-         intel: "8de08bd435ef8ae0fc5fbd6acefa9c68e62fb898c5ae0fbdacd26853bea9d4d6"
-  url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_darwin_#{arch}.dmg",
-      verified: "hashicorp.com/vagrant/"
+
+  on_macos do
+    arch arm: "arm64", intel: "amd64"
+    sha256 arm: "8de08bd435ef8ae0fc5fbd6acefa9c68e62fb898c5ae0fbdacd26853bea9d4d6",
+           intel: "8de08bd435ef8ae0fc5fbd6acefa9c68e62fb898c5ae0fbdacd26853bea9d4d6"
+    url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_darwin_#{arch}.dmg",
+        verified: "hashicorp.com/vagrant/"
+
+    pkg "vagrant.pkg"
+
+    uninstall script: {
+      executable: "uninstall.tool",
+      input: ["Yes"],
+      sudo:  true,
+    },
+    pkgutil: "com.vagrant.vagrant"
+
+    zap trash: "~/.vagrant.d"
+  end
+
   name "Vagrant"
   desc "Development environment"
   homepage "https://www.vagrantup.com/"
@@ -16,16 +31,5 @@ cask "hashicorp-vagrant" do
     url "https://github.com/hashicorp/vagrant"
     strategy :git
   end
-
-  pkg "vagrant.pkg"
-
-  uninstall script: {
-    executable: "uninstall.tool",
-    input: ["Yes"],
-    sudo:  true,
-  },
-  pkgutil: "com.vagrant.vagrant"
-
-  zap trash: "~/.vagrant.d"
 
 end
