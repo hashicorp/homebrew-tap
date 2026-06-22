2fab352f104d0f08d0da6aa42368a6b215abddd0
Brandon Croft <brandon.croft@gmail.com>
2026-06-22 16:28:39 -0600

    bump tfctl to 0.3.0

--- Formula/tfctl.rb
+++ Formula/tfctl.rb
@@ -4,31 +4,31 @@
 class Tfctl < Formula
   desc ""
   homepage "https://www.terraform.io"
-  version "0.2.0"
+  version "0.3.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/tfctl/0.2.0/tfctl_0.2.0_darwin_amd64.zip"
-    sha256 "f4b82849d15dd3b7cef94c9c440d1fdadc6d64e0a56b0a1bf2bc3c247e674a8b"
+    url "https://releases.hashicorp.com/tfctl/0.3.0/tfctl_0.3.0_darwin_amd64.zip"
+    sha256 "a12de01e640cb9dea6080b7e8a2cdb74c5efe466f3ff6ba19c520c03d383dac9"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.hashicorp.com/tfctl/0.2.0/tfctl_0.2.0_darwin_arm64.zip"
-    sha256 "70f781f8c718f0289e8ee6c5cba55a56e54371183795d1277948849e38f85965"
+    url "https://releases.hashicorp.com/tfctl/0.3.0/tfctl_0.3.0_darwin_arm64.zip"
+    sha256 "045a5d7ebf2913006213b485353acf7c832bbf28e1cf3a6a34313dd761d4f535"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.hashicorp.com/tfctl/0.2.0/tfctl_0.2.0_linux_amd64.zip"
-    sha256 "ed7dd6c49ac906f3180f9f1c4ba440f19e662fac6508f4e78261772b688178ed"
+    url "https://releases.hashicorp.com/tfctl/0.3.0/tfctl_0.3.0_linux_amd64.zip"
+    sha256 "5526b438bb66c5bc872f55c4bbb7325bb3e9d162ddd392eed3d60f407598cf62"
   end
 
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/tfctl/0.2.0/tfctl_0.2.0_linux_arm.zip"
-    sha256 "ed7dd6c49ac906f3180f9f1c4ba440f19e662fac6508f4e78261772b688178ed"
+    url "https://releases.hashicorp.com/tfctl/0.3.0/tfctl_0.3.0_linux_arm.zip"
+    sha256 "5526b438bb66c5bc872f55c4bbb7325bb3e9d162ddd392eed3d60f407598cf62"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.hashicorp.com/tfctl/0.2.0/tfctl_0.2.0_linux_arm64.zip"
-    sha256 "9e456947a1363c8dad964695d38af3b2c6da1ab2b56340b04d1480b82540ab7e"
+    url "https://releases.hashicorp.com/tfctl/0.3.0/tfctl_0.3.0_linux_arm64.zip"
+    sha256 "1060b2903cd8f9cb0af5c878cf89790e0f588b735b6715d2eeadd52c5a71f8c5"
   end
 
   conflicts_with "tfctl"
