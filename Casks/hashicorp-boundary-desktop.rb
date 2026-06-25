c53a2d059493a977c405ada2cc3df52834261b99
Ashley Connor <aconnor@hashicorp.com>
2026-06-25 10:33:24 -0700

    fix: Update Cask templater to handle on_macos block
    - Linux taps emit an error/warning due to a missing SHA256 hash in the
      formula for that platform.
    - A on_macos guard prevents this but was being reverted by the template,
      the template has been updated for Casks to include this guard.
    - NOTE: If we add Linux Casks in the future this will need to be
      updated.

--- Casks/hashicorp-boundary-desktop.rb
+++ Casks/hashicorp-boundary-desktop.rb
@@ -7,7 +7,7 @@ cask "hashicorp-boundary-desktop" do
   on_macos do
     arch arm: "arm64", intel: "amd64"
     sha256 arm: "636f4564d203f7d69de15c2655d167e4b7eef3455464ab14f4ba38f86e11edc2",
-          intel: "8773effd0457b337b8adca85608442be29991f362099c9d915758b6b2e4a741a"
+           intel: "8773effd0457b337b8adca85608442be29991f362099c9d915758b6b2e4a741a"
     url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_#{arch}.dmg",
         verified: "hashicorp.com/boundary-desktop/"
 
