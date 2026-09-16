b62c879c185e0714f0742939b1f28c8a2f32d4a1
hc-github-team-es-release-engineering <82989873+hc-github-team-es-release-engineering@users.noreply.github.com>
2026-09-16 14:58:08 -0700

    Bump boundary-desktop to 2.6.3

--- Casks/hashicorp-boundary-desktop.rb
+++ Casks/hashicorp-boundary-desktop.rb
@@ -2,14 +2,13 @@
 # SPDX-License-Identifier: MPL-2.0
 
 cask "hashicorp-boundary-desktop" do
-  version "2.6.2"
+  version "2.6.3"
 
   on_macos do
     arch arm: "arm64", intel: "amd64"
 
-    sha256 arm:   "183ca42ce04b6cbf98661c54bc6bc79a1c8da401c0bcb2a4ef5b9b69bdfb119a",
-           intel: "05640df4f9bb297c05f03be23f96a7d1f9c6a83e2cf4e9ca530bafee71e73880"
-
+    sha256 arm:   "4b3882b94846806c34d569281b5a864a657af5ca1384cd8aae5721ee55396cae",
+           intel: "f42423da63c7dc656aab2245e75aba94f8b420652d0660e2cf1902b82f3c283b"
     url "https://releases.hashicorp.com/boundary-desktop/#{version}/boundary-desktop_#{version}_darwin_#{arch}.dmg"
 
     app "Boundary.app"
